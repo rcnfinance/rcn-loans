84ebf501ded3927c67a7745db6b6c8e3bd1a1676
Ignacio Perez Roca <ignacio.perezroca@gmail.com>
2018-05-11 14:09:56 -0300

    Open Loans UI

--- src/app/services/contracts.service.ts
+++ src/app/services/contracts.service.ts
@@ -28,9 +28,9 @@ function curateLoans(loans: Loan[]): Loan[] {
   return loans.filter(loan => {
     return loan.annualInterest < 1000 &&
       loan.annualPunitoryInterest < 1000 &&
-      loan.amount < 1000000 && 
-      loan.amount > 0.00001
-  })
+      loan.amount < 1000000 &&
+      loan.amount > 0.00001;
+  });
 }
 
 @Injectable()
