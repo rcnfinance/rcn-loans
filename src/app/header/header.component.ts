f57962445ba5e4cb51c1204fb8cdf5c79a596393
Ignacio Perez Roca <ignacio.perezroca@gmail.com>
2018-05-29 18:30:55 -0300

    Header profile refurbish

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,6 +5,7 @@ import { SharedModule } from './../shared/shared.module';
 import { DialogApproveContractComponent } from '../dialogs/dialog-approve-contract/dialog-approve-contract.component';
 import { MatDialog, MatSnackBar, MatDialogRef } from '@angular/material';
 import { Web3Service } from '../services/web3.service';
+import { Router } from '@angular/router';
 
 // App Component
 
@@ -16,10 +17,32 @@ import { Web3Service } from '../services/web3.service';
 export class HeaderComponent implements OnInit, AfterViewInit {
   account: string;
   makeRotate = false;
-
-  constructor(public dialog: MatDialog, private web3: Web3Service) {}
+  profile: boolean;
+  constructor(
+    public dialog: MatDialog,
+    private web3: Web3Service,
+    private router: Router,
+  ) {
+    console.log(this.profile);
+  }
   @ViewChild('tref', {read: ElementRef}) tref: ElementRef;
 
+  handleProfile() {
+    this.profile = !this.profile;
+    if (this.profile) {
+      this.router.navigate(['/profile/']).then(nav => {
+        console.log(nav); // true if navigation is successful
+      }, err => {
+        console.log(err); // when there's an error
+      });
+    } else {
+      this.router.navigate(['/requests/']).then(nav => {
+        console.log(nav); // true if navigation is successful
+      }, err => {
+        console.log(err); // when there's an error
+      });
+    }
+  }
   openDialog() {
     const dialogRef: MatDialogRef<DialogApproveContractComponent> = this.dialog.open(DialogApproveContractComponent, {
       width: '800px'
