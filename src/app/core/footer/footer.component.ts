89865a97441d162badee9b435ea8aed09921c0dd
Ignacio Perez Roca <ignacio.perezroca@gmail.com>
2018-06-04 12:29:19 -0300

    Pull master

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
   ngOnInit() {
   }
 
