dd36bf0051456da98430a93bb719db085da6ee80
RCN <rcn@RCNs-MacBook-Pro.local>
2018-06-07 10:57:07 -0300

    Sidebar Service

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+
+@Injectable()
+export class SidebarService {
+  isOpen$: BehaviorSubject<boolean> = new BehaviorSubject(true);
+}
