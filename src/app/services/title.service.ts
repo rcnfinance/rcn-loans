5578808d048a5ee88f5adcc3c10b205ba87ccce2
Ignacio <ignacio@ripiocredit.network>
2018-06-21 19:35:12 -0300

    Titles services

---
+++ src/app/services/title.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class TitleService {
+  private titleSource = new BehaviorSubject('Requests');
+  currentTitle = this.titleSource.asObservable();
+
+  changeTitle(title: string) {
+    this.titleSource.next(title);
+  }
+
+}
