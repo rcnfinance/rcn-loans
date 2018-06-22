dc4d47076b1900bf9d885752df9eb73383c00913
Ignacio <ignacio@ripiocredit.network>
2018-06-22 17:15:36 -0300

    Dialog Founds

---
+++ src/app/dialogs/dialog-insufficient-founds/dialog-insufficient-founds.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-insufficient-founds',
+  templateUrl: './dialog-insufficient-founds.component.html',
+  styleUrls: ['./dialog-insufficient-founds.component.scss']
+})
+export class DialogInsufficientFoundsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
