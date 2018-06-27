33c8c4523168af3561973fec889f8a335ac92917
Agustin Esteban Aguilar <agustin@bitpagos.co>
2018-06-27 16:09:09 +0200

    Withdraw transaction

--- src/app/content-wrapper/content-wrapper.component.ts
+++ src/app/content-wrapper/content-wrapper.component.ts
@@ -9,6 +9,7 @@ import { SidebarService } from '../services/sidebar.service';
 import { Web3Service, Type } from '../services/web3.service';
 import { ContractsService } from '../services/contracts.service';
 import BigNumber from 'bignumber.js';
+import { Tx, TxService } from '../tx.service';
 
 @Component({
   selector: 'app-content-wrapper',
@@ -34,10 +35,13 @@ export class ContentWrapperComponent implements OnInit {
   extensionToggled = false; // Balance extension toggled
   navmobileToggled = false; // Nav Mobile toggled
 
+  pendingWithdraw: Tx;
+
   constructor(
     private sidebarService: SidebarService, // Navbar Service
     private web3Service: Web3Service,
     private contractService: ContractsService,
+    private txService: TxService,
     public dialog: MatDialog,
   ) {}
 
@@ -57,7 +61,15 @@ export class ContentWrapperComponent implements OnInit {
     this.sidebarService.toggleService(this.navToggle = true);
     this.sidebarService.extensionService(this.extensionToggled = false);
   }
-
+  async clickWithdraw() {
+    if (!this.withdrawEnabled) {
+      window.open(environment.network.explorer.tx.replace('${tx}', this.pendingWithdraw.tx));
+    } else {
+      const tx = await this.contractService.withdrawFunds(this.loansWithBalance);
+      this.txService.registerWithdrawTx(tx, environment.contracts.basaltEngine, this.loansWithBalance);
+      this.loadPendingWithdraw();
+    }
+  }
   // Get Balance
   get hasAccount(): boolean {
     return this.account !== undefined;
@@ -74,6 +86,11 @@ export class ContentWrapperComponent implements OnInit {
     }
     return this.removeTrailingZeros((this.weiAvailable / this.ethWei).toFixed(18));
   }
+  get withdrawEnabled(): boolean {
+    return this.loansWithBalance !== undefined
+      && this.loansWithBalance.length !== 0
+      && this.pendingWithdraw === undefined;
+  }
   private removeTrailingZeros(value) {
     value = value.toString();
     if (value.indexOf('.') === -1) {
@@ -84,24 +101,27 @@ export class ContentWrapperComponent implements OnInit {
     }
     return value;
   }
-  loadLender() {
+  private loadPendingWithdraw() {
+    this.pendingWithdraw = this.txService.getLastWithdraw(environment.contracts.basaltEngine, this.loansWithBalance);
+  }
+  private loadLender() {
     this.web3Service.getAccount().then((resolve: string) => {
       this.lender = resolve;
     });
   }
-  loadRcnBalance() {
+  private loadRcnBalance() {
     this.contractService.getUserBalanceRCN().then((balance: number) => {
       this.rcnBalance = balance;
     });
   }
-  loadWithdrawBalance() {
+  private loadWithdrawBalance() {
     this.contractService.getPendingWithdraws().then((result: [number, number[]]) => {
       this.weiAvailable = result[0];
       this.loansWithBalance = result[1];
+      this.loadPendingWithdraw();
     });
   }
-
-  ngOnInit(): void {
+  ngOnInit() {
      // Navbar toggled
     this.sidebarService.currentToggle.subscribe(navToggle => this.navToggle = navToggle);
     this.sidebarService.currentExtension.subscribe(extensionToggled => this.extensionToggled = extensionToggled);
