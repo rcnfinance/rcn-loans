45256b87c510d09a7693cfacecc8919207d05b5e
Agustin Esteban Aguilar <agustin@bitpagos.co>
2018-07-10 18:33:44 +0200

    Added new Decentraland mortgage creator

--- src/environments/cosigners.ts
+++ src/environments/cosigners.ts
@@ -9,5 +9,12 @@ export const cosignerOptions: CosignerProvider[] = [
         '0x2bdf545935d4264cbb7457e97d69b6b86458eb64',
         '0x80faa2b517b84a5aec1078d3600eab4c0b3aff56',
         './proxy_decentraland/'
+    ),
+    new DecentralandCosignerProvider(
+        environment.contracts.basaltEngine,
+        '0xea06746f1bd82412f9f243f6bee0b8194d67a67d',
+        '0x59ccfc50bd19dcd4f40a25459f2075084eebc11e',
+        '0x80faa2b517b84a5aec1078d3600eab4c0b3aff56',
+        './proxy_decentraland/'
     )
 ];
