20d68030bbda2d58b95057fa214592a1f95228db
NPR <ignacio.perezroca@gmail.com>
2018-07-25 19:21:18 -0300

    Timeline component

--- src/app/views/loan-detail/transaction-history/transaction-history.component.ts
+++ src/app/views/loan-detail/transaction-history/transaction-history.component.ts
@@ -6,7 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transaction-history.component.scss']
 })
 export class TransactionHistoryComponent implements OnInit {
-
+  icon: string = 'trending_up';
+  timeline: any[] = [
+    {
+      'icon': 'trending_up',
+      'title': 'Lent',
+      'color': 'blue',
+      'inserted': false,
+      'powers': [
+        'Radiation resistance',
+        'Turning tiny',
+        'Radiation blast'
+      ]
+    },
+    {
+      'icon': 'code',
+      'title': 'Pay',
+      'color': 'green',
+      'inserted': true,
+      'powers': [
+        'Radiation resistance',
+        'Turning tiny',
+        'Radiation blast'
+      ]
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
