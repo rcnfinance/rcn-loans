7b5628bacde31d62ede151ae71f2efbdcc0cd2a4
Ignacio <ignacio@ripiocredit.network>
2018-07-26 14:33:43 -0300

    Timeline JSON added

--- src/app/views/loan-detail/transaction-history/transaction-history.component.ts
+++ src/app/views/loan-detail/transaction-history/transaction-history.component.ts
@@ -9,27 +9,34 @@ export class TransactionHistoryComponent implements OnInit {
   icon: string = 'trending_up';
   timeline: any[] = [
     {
+      'materialClass': 'material-icons',
       'icon': 'trending_up',
       'title': 'Lent',
       'color': 'blue',
-      'inserted': false,
-      'powers': [
-        'Radiation resistance',
-        'Turning tiny',
-        'Radiation blast'
-      ]
+      'inserted': false
     },
     {
-      'icon': 'code',
+      'awesomeClass': 'fas fa-coins',
       'title': 'Pay',
       'color': 'green',
-      'inserted': true,
-      'powers': [
-        'Radiation resistance',
-        'Turning tiny',
-        'Radiation blast'
-      ]
+      'hexa': '#71b464',
+      'inserted': true
     },
+    {
+      'materialClass': 'material-icons',
+      'icon': 'trending_up',
+      'title': 'Lent',
+      'color': 'white',
+      'hexa': '#ffffff',
+      'inserted': true
+    },
+    {
+      'materialClass': 'material-icons',
+      'icon': 'delete',
+      'title': 'Destroyed',
+      'color': 'red',
+      'inserted': false
+    }
   ];
   constructor() { }
 
