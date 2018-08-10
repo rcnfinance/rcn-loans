2eef0308ac8baec80c052ef5e41568d1a2297830
rotcivegaf <victorfage@gmail.com>
2018-08-10 17:03:49 -0300

    change function name

--- src/app/utils/validate.ts
+++ src/app/utils/validate.ts
@@ -1,10 +1,12 @@
-export function loanParameters(_amount, _duration, _firstPayment, _interestRate): boolean {
+export function validateLoanParameters(_amount, _duration, _firstPayment, _interestRate): boolean {
   if(_amount <= 0 || _duration <= 0 || _firstPayment <= 0 || _interestRate <= 0) {
     // TODO Show an error
+    console.log("ERROR");
     return false;
   }
   if(_duration <= _firstPayment){
     // TODO Show an error
+    console.log("ERROR");
     return false;
   }
   return true;
