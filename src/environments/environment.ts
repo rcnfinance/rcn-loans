826e484aebe82024303baf1d67ad47c1f2892b58
rotcivegaf <victorfage@gmail.com>
2018-08-15 18:59:11 -0300

    fix url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -42,6 +42,6 @@ export const environment = {
     '0xdc5fdc6d0c24573c7e2ac3896ab10e376be6da86': Agent.RipioCreator,
   },
   gasPriceAPIs: {
-    gasStation: 'https://ethgasstation.info/jso2n/ethgasAPI.json'
+    gasStation: 'https://ethgasstation.info/json/ethgasAPI.json'
   }
 };
