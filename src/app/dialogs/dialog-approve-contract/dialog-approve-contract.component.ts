939012e0f1f773ada103343646f4d1830a62b598
Agustin Esteban Aguilar <agustin@bitpagos.co>
2018-08-17 21:44:55 +0200

    Add approve RCN event

--- src/app/dialogs/dialog-approve-contract/dialog-approve-contract.component.ts
+++ src/app/dialogs/dialog-approve-contract/dialog-approve-contract.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 // App Component
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { Web3Service } from '../../services/web3.service';
-import { Utils } from '../../utils/utils';
 import { ContractsService } from '../../services/contracts.service';
+import { EventsService, Category } from '../../services/events.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-dialog-approve-contract',
@@ -18,6 +19,7 @@ export class DialogApproveContractComponent implements OnInit {
   constructor(
     private web3Service: Web3Service,
     private contracts: ContractsService,
+    private eventsService: EventsService,
     public dialog: MatDialog,
     private dialogRef: MatDialogRef<DialogApproveContractComponent>
   ) { }
@@ -37,21 +39,35 @@ export class DialogApproveContractComponent implements OnInit {
   }
   clickCheck() {
     let action;
+    let actionCode;
+
     if (this.isApproved) {
+      actionCode = 'dissaprove';
       action = this.contracts.dissaproveEngine();
     } else {
+      actionCode = 'approve';
       action = this.contracts.approveEngine();
     }
 
+    this.eventsService.trackEvent(
+      'click-' + actionCode + '-basalt-rcn',
+      Category.Account,
+      environment.contracts.basaltEngine
+    );
+
     action.then(() => {
       this.loadApproved().then(() => {
+        this.eventsService.trackEvent(
+          actionCode + '-basalt-rcn',
+          Category.Account,
+          environment.contracts.basaltEngine
+        );
         if (this.autoClose) {
           this.dialogRef.close(this.isApproved);
         }
       });
     });
   }
-  
   ngOnInit() {
     this.loadLender();
     this.loadApproved();
