c9745caa7903a4b7600c00658d91c7cc91889dfa
Agustin Esteban Aguilar <agustin@bitpagos.co>
2018-08-23 14:55:43 +0200

    Fix double promise

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -94,3 +94,12 @@ export class Utils {
     return value;
   }
 }
+
+export function promisify(func: any, args: any): Promise<any> {
+  return new Promise((res, rej) => {
+    func(...args, (err: any, data: any) => {
+      if (err) { return rej(err); }
+      return res(data);
+    });
+  });
+}
