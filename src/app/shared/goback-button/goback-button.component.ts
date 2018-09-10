851f56e6146eaa2e61e6898b537444adffa3769f
Ignacio <ignacio@ripiocredit.network>
2018-09-10 12:20:07 -0300

    Code cleaning

--- src/app/shared/goback-button/goback-button.component.ts
+++ src/app/shared/goback-button/goback-button.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 
 @Component({
@@ -10,7 +9,6 @@ import { Location } from '@angular/common';
 export class GobackButtonComponent {
 
   constructor(
-    private router: Router,
     private location: Location
   ) {}
 
