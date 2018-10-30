c7f1e506d62f6f55f80326ece3f4d293496079cf
Agustin Aguilar <agustin@ripiocredit.network>
2018-10-30 12:24:29 +0100

    Removed code comments

--- src/app/directives/windows-height.directive.ts
+++ src/app/directives/windows-height.directive.ts
@@ -7,17 +7,10 @@ export class WindowsHeightDirective {
   screenHeight: any = (document.body.scrollHeight) + 'px';
   constructor() {
     this.screenHeight = (document.body.scrollHeight) + 'px';
-    // console.log(this.screenHeight);
   }
   @HostBinding ('style.height') height: any = this.screenHeight;
   @HostBinding ('style.minHeight') minHeight: any = '900px';
 
-  @HostListener ('mouseenter') mouseover(eventData: Event) {
-    // this.height = '900px';
-  }
-  @HostListener ('mouseleave') mouseleave(eventData: Event) {
-    // this.height = '3000px';
-  }
-
-
+  @HostListener ('mouseenter') mouseover(eventData: Event) {}
+  @HostListener ('mouseleave') mouseleave(eventData: Event) {}
 }
