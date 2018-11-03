99523da00f0b6d4b7db5f6278e9a3fc86ac0c8da
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-03 02:24:23 +0100

    Removed blacklisted and unused imports

--- src/app/services/commits.service.ts
+++ src/app/services/commits.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/Rx';
-import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
 import { Commit } from '../models/commit.model';
 import { environment } from '../../environments/environment';
-import { Loan } from '../models/loan.model';
 
 @Injectable()
 export class CommitsService {
