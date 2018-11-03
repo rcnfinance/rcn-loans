1223f2ef75f6aebde84f0b3a7457d11caf98b6df
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-03 17:35:02 +0100

    Remove unused onInit

--- src/app/shared/pay-form/pay-form.component.ts
+++ src/app/shared/pay-form/pay-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { NgForm } from '@angular/forms';
   templateUrl: './pay-form.component.html',
   styleUrls: ['./pay-form.component.scss']
 })
-export class PayFormComponent implements OnInit {
+export class PayFormComponent {
   @Output() submitTransfer = new EventEmitter<any>();
 
   constructor() {}
@@ -15,8 +15,5 @@ export class PayFormComponent implements OnInit {
     event.preventDefault();
     const to = form.value;
     this.submitTransfer.emit(to);
-  }
-
-  ngOnInit() {
   }
 }
