1d54078e21648fe82a13cad59b5daad2bbc427fc
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-03 20:07:04 +0100

    Removed duplicated cosigner entry

--- src/environments/cosigners.ts
+++ src/environments/cosigners.ts
@@ -5,13 +5,6 @@ import { CosignerProvider } from '../app/providers/cosigner-provider';
 export const cosignerOptions: CosignerProvider[] = [
   new DecentralandCosignerProvider(
         environment.contracts.basaltEngine,
-        '0x31ebb4ffd5e34acfc87ea21a0c56157188f3f0e1',
-        '0x0e4c24f71c8679b8af8e5a22aac3816e2b23f1cc',
-        '0x80faa2b517b84a5aec1078d3600eab4c0b3aff56',
-        'https://api.decentraland.zone/v1/'
-    ),
-  new DecentralandCosignerProvider(
-        environment.contracts.basaltEngine,
         '0x9ABf1295086aFA0E49C60e95c437aa400c5333B8',
         '0x90263Ea5C57Dc6603CA7202920735A6E31235bB9',
         '0x8e5660b4Ab70168b5a6fEeA0e0315cb49c8Cd539',
