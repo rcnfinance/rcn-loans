ab0c59b12697fdabdd75069f973b23a41fd70ff9
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-03 02:30:38 +0100

    Removed unused method

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -5,13 +5,6 @@ export class Utils {
     return 311040000000000 / raw;
   }
 
-  static newGuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      const r = Math.random() * 16 | 0, v = c === 'x' ? r : ( r & 0x3 | 0x8 );
-      return v.toString(16);
-    });
-  }
-
   static hexToAscii(str) {
     const hexString = str;
     let strOut = '';
