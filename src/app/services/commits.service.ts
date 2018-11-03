482ef9b583c5e9bd9e0a2737808d4368027cf26d
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-03 02:26:02 +0100

    Fix tslint identation

--- src/app/services/commits.service.ts
+++ src/app/services/commits.service.ts
@@ -9,7 +9,7 @@ export class CommitsService {
   constructor(private http: Http) {}
 
   async getCommits(id: number): Promise<Commit[]> {
-  	const url = environment.rcn_node.loan.replace('$id', id.toString());
+    const url = environment.rcn_node.loan.replace('$id', id.toString());
     const response = await this.http.get(url).toPromise();
     const data = response.json();
     return data.content.commits;
