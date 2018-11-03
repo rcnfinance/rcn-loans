47382832bd8170746c1fd6fd9e575d8d2a5811e8
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-03 17:34:29 +0100

    Removed unused is address method and inport web3service

--- src/app/shared/pay-form/pay-form.component.ts
+++ src/app/shared/pay-form/pay-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Web3Service } from '../../services/web3.service';
 
 @Component({
   selector: 'app-pay-form',
@@ -9,25 +8,15 @@ import { Web3Service } from '../../services/web3.service';
 })
 export class PayFormComponent implements OnInit {
   @Output() submitTransfer = new EventEmitter<any>();
-  invalidAddress = false;
 
-  constructor(
-    private web3Service: Web3Service
-  ) {}
+  constructor() {}
 
  onSubmit(event: any, form: NgForm) {
     event.preventDefault();
     const to = form.value;
     this.submitTransfer.emit(to);
   }
-  private isAddress(address: string): boolean {
-    const web3 = this.web3Service.web3;
-    if (web3.utils !== undefined) {
-      return web3.utils.isAddress(address);
-    } else {
-      return web3.isAddress(address);
-    }
-  }
+
   ngOnInit() {
   }
 }
