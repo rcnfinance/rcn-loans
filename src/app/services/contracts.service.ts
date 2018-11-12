a3d2c73c798ecec996f262d5dfa3f8b56583eba1
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-12 22:39:02 +0100

    Send account web3 ops calls

--- src/app/services/contracts.service.ts
+++ src/app/services/contracts.service.ts
@@ -86,8 +86,11 @@ export class ContractsService {
     const rcnAddress = this._rcnContractAddress;
     const engineAddress = this._rcnEngineAddress;
     return new Promise((resolve, reject) => {
-      const _web3 = this.web3.web3;
-      this._rcnContract.approve(this._rcnEngineAddress, _web3.toWei(10 ** 32), { from: account }, function (err, result) {
+      const web3 = this.web3.opsWeb3;
+      this.loadAltContract(
+        web3,
+        this._rcnContract
+      ).approve(this._rcnEngineAddress, web3.toWei(10 ** 32), { from: account }, function (err, result) {
         if (err != null) {
           reject(err);
         } else {
@@ -104,7 +107,11 @@ export class ContractsService {
     const rcnAddress = this._rcnContractAddress;
     const engineAddress = this._rcnEngineAddress;
     return new Promise((resolve, reject) => {
-      this._rcnContract.approve(this._rcnEngineAddress, 0, { from: account }, function (err, result) {
+      const web3 = this.web3.opsWeb3;
+      this.loadAltContract(
+        web3,
+        this._rcnContract
+      ).approve(this._rcnEngineAddress, 0, { from: account }, function (err, result) {
         if (err != null) {
           reject(err);
         } else {
@@ -133,7 +140,6 @@ export class ContractsService {
   }
 
   async lendLoan(loan: Loan): Promise<string> {
-    const account = await this.web3.getAccount();
     const pOracleData = this.getOracleData(loan);
 
     const cosigner = this.cosignerService.getCosigner(loan);
@@ -146,19 +152,31 @@ export class ContractsService {
     }
     const oracleData = await pOracleData;
     return new Promise((resolve, reject) => {
-      this._rcnEngine.lend(loan.id, oracleData, cosignerAddr, cosignerData, { from: account }, function(err, result) {
-        if (err != null) {
-          reject(err);
-        } else {
-          resolve(result);
+      this.loadAltContract(
+        this.web3.opsWeb3,
+        this._rcnEngine
+      ).lend(
+        loan.id,
+        oracleData,
+        cosignerAddr,
+        cosignerData,
+        { from: this.web3.getAccount() },
+        function(err, result) {
+          if (err != null) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
         }
-      });
+      );
     }) as Promise<string>;
   }
   async transferLoan(loan: Loan, to: string): Promise<string> {
-    const account = await this.web3.getAccount();
     return new Promise((resolve, reject) => {
-      this._rcnEngine.transfer(to, loan.id, { from: account }, function(err, result) {
+      this.loadAltContract(
+        this.web3.opsWeb3,
+        this._rcnEngine
+      ).transfer(to, loan.id, function(err, result) {
         if (err != null) {
           reject(err);
         }
@@ -169,7 +187,10 @@ export class ContractsService {
   async withdrawFunds(loans: number[]): Promise<string> {
     const account = await this.web3.getAccount();
     return new Promise((resolve, reject) => {
-      this._rcnEngine.withdrawalList(loans, account, (err, result) => {
+      this.loadAltContract(
+        this.web3.opsWeb3,
+        this._rcnEngine
+      ).withdrawalList(loans, account, (err, result) => {
         if (err != null) {
           reject(err);
         }
@@ -198,7 +219,7 @@ export class ContractsService {
     if (data === undefined) {
       throw new Error('Oracle did not provide data');
     }
-
+    web3.eth.contract(contract.abi).at(contract.address)
     return data;
   }
   async getLoan(id: number): Promise<Loan> {
@@ -286,6 +307,9 @@ export class ContractsService {
         resolve(this.readPendingWithdraws(loans));
       });
     }) as Promise<[number, number[]]>;
+  }
+  private loadAltContract(web3: any, contract: any): any {
+    return web3.eth.contract(contract.abi).at(contract.address);
   }
   private parseLoansBytes(bytes: any): Loan[] {
     const loans = [];
