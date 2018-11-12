caac4b6f28d61b31c7da925fe596a35100fb01d7
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-12 22:39:11 +0100

    Load account on click lend

--- src/app/shared/lend-button/lend-button.component.ts
+++ src/app/shared/lend-button/lend-button.component.ts
@@ -33,7 +33,6 @@ import { DialogClientAccountComponent } from '../../dialogs/dialog-client-accoun
 export class LendButtonComponent implements OnInit {
   @Input() loan: Loan;
   pendingTx: Tx = undefined;
-  account: string;
   lendEnabled: Boolean;
   opPending = false;
   horizontalPosition: MatSnackBarHorizontalPosition = 'center';
@@ -52,7 +51,12 @@ export class LendButtonComponent implements OnInit {
   async handleLend(forze = false) {
     if (this.opPending && !forze) { return; }
 
-    if (this.account === undefined) {
+    if (!this.web3Service.loggedIn) {
+      if (await this.web3Service.requestLogin()) {
+        this.handleLend();
+        return;
+      }
+
       this.dialog.open(DialogClientAccountComponent);
       return;
     }
@@ -209,9 +213,6 @@ export class LendButtonComponent implements OnInit {
 
   ngOnInit() {
     this.retrievePendingTx();
-    this.web3Service.getAccount().then((account) => {
-      this.account = account;
-    });
     this.countriesService.lendEnabled().then((lendEnabled) => {
       this.lendEnabled = lendEnabled;
     });
