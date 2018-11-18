66e22dcf51dca205872e430a6a40950834df6296
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-18 17:56:02 +0100

    Add navigate to detail test

--- e2e/loan-detail.test.ts
+++ e2e/loan-detail.test.ts
@@ -1,3 +1,4 @@
+// tslint:disable:max-line-length
 
 import 'jest';
 
@@ -13,6 +14,20 @@ beforeAll(async (done) => {
   done();
 });
 
+test('Shoud navigate to detail', async () => {
+  const page = await browser.newPage();
+  await page.goto('http://localhost:4200/address/0x791bcc53d4adbfeb6f471f6241262b9a9021ea3f');
+  await page.waitFor(1000);
+  await screenshot(page, 'lender-page');
+  await page.$eval(
+    '#page-content-wrapper > app-address > div > div:nth-child(2) > div > app-loan-card > div > div > app-detail-button > a',
+    e => e.click()
+  );
+  await page.waitFor(1000);
+  expect(await page.evaluate(() => location.href)).toBe('http://localhost:4200/loan/2');
+  await page.close();
+});
+
 test('Should display detail of loan', async () => {
   // Open profile
   const page = await browser.newPage({ width: 1366, height: 768 });
@@ -24,7 +39,6 @@ test('Should display detail of loan', async () => {
 
   // Should display paid status
   const statusText = await page.$eval(
-    // tslint:disable-next-line:max-line-length
     '#page-content-wrapper > app-loan-detail > div > div.loan-description.flex-container > div.left > div.avatar-information > div:nth-child(2) > app-avatar-title > div > div.txt-container.flex-item > div.title.ellipsis',
     e => e.innerText
   );
@@ -32,7 +46,6 @@ test('Should display detail of loan', async () => {
 
   // Should display lent 400
   const lentText = await page.$eval(
-    // tslint:disable-next-line:max-line-length
     '#page-content-wrapper > app-loan-detail > div > div.loan-description.flex-container > div.left > div:nth-child(2) > app-conversion-graphic > app-body-list > div > div:nth-child(1)',
     e => e.innerText
   );
@@ -40,7 +53,6 @@ test('Should display detail of loan', async () => {
 
   // Should display paid ammount
   const paidText = await page.$eval(
-    // tslint:disable-next-line:max-line-length
     '#page-content-wrapper > app-loan-detail > div > div.loan-description.flex-container > div.left > div:nth-child(3) > app-conversion-graphic > app-body-list > div > div:nth-child(1)',
     e => e.innerText
   );
@@ -54,6 +66,8 @@ test('Should display detail of loan', async () => {
   );
 
   expect(lenderText).toContain('0x791bcc53d4adbfeb6f471f6241262b9a9021ea3f');
+
+  await page.close();
 }, 14000);
 
 afterAll(async () => {
