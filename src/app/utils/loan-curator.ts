69d83304c2b0a87f0a53ac5bd2ffd46d3179210a
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-20 21:07:47 +0100

    Add Old Ripio oracle to Ropsten blacklist

--- src/app/utils/loan-curator.ts
+++ src/app/utils/loan-curator.ts
@@ -1,8 +1,22 @@
 import { Loan } from '../models/loan.model';
+import { environment } from '../../environments/environment';
 
 export class LoanCurator {
   static curateLoans(loans: Loan[]): Loan[] {
     return loans.filter(loan => {
+      // Check blacklist
+      if (environment.blacklist) {
+        let blacklisted = false;
+        environment.blacklist.forEach(element => {
+          if (element.forbidden.includes(loan[element.key])) {
+            blacklisted = true;
+            return;
+          }
+        });
+        if (blacklisted) { return false; }
+      }
+
+      // Check common wrong values
       return loan.annualInterest < 1000 &&
             loan.annualPunitoryInterest < 1000 &&
             loan.amount < 1000000 &&
