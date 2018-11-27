9616b46ce2e24cd16f0a9ac8ba1b1b2b790023be
Ignacio <ignacio@ripiocredit.network>
2018-11-27 14:30:02 -0300

    Refactor Address

--- src/app/views/address/address.module.ts
+++ src/app/views/address/address.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// App Modules
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
+import { SharedModule } from '../../shared/shared.module';
+import { MaterialModule } from './../../material/material.module';
+// App Services
+import { ContractsService } from './../../services/contracts.service';
+import { AvailableLoansService } from '../../services/available-loans.service';
+// App Component
+import { AddressComponent } from './address.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgxSpinnerModule,
+    MaterialModule,
+    SharedModule
   ],
-  declarations: []
+  declarations: [
+    AddressComponent
+  ],
+  providers: [
+    NgxSpinnerService,
+    ContractsService,
+    AvailableLoansService
+  ],
+  exports: [
+    AddressComponent
+  ]
 })
 export class AddressModule { }
