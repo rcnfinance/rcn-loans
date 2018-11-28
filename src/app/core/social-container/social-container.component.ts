1b925d5e13167aff0db4da17add33ae9fe1d5c16
Ignacio <ignacio@ripiocredit.network>
2018-11-28 13:17:33 -0300

    Shared Module socialContainer moved to Core Module

---
+++ src/app/core/social-container/social-container.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment.prod';
+
+@Component({
+  selector: 'app-social-container',
+  templateUrl: './social-container.component.html',
+  styleUrls: ['./social-container.component.scss']
+})
+export class SocialContainerComponent implements OnInit {
+  linkContract: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.linkContract = environment.network.explorer.address.replace('${address}', environment.contracts.basaltEngine);
+  }
+}
