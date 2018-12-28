0af9dc8274e1c8322ad0d60c46c467775a0817f9
Ignacio <ignacio@ripiocredit.network>
2018-12-27 23:25:00 -0300

    Tx href to Notification added

--- src/app/core/header/notifications/notification-item/notification-item.component.ts
+++ src/app/core/header/notifications/notification-item/notification-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Notification } from '../../../../models/notification.model';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-notification-item',
@@ -10,8 +11,15 @@ export class NotificationItemComponent implements OnInit {
   @Input() notification: Notification;
 
   progressbarMode = 'query';
+  urlAddress: String;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (environment.envName === 'dev') {
+      this.urlAddress = 'https://ropsten.etherscan.io/tx/';
+    } else {
+      this.urlAddress = 'https://etherscan.io/tx/';
+    }
+  }
 }
