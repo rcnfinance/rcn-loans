4d21efd9931fc8ffa6576d338b54ad05290a37a5
Nicolas <menendeznm@gmail.com>
2019-01-22 11:06:42 -0300

    Fix bug, transacions in notification component were showing pending although they were already confirmed

--- src/app/core/header/notifications/notifications.component.ts
+++ src/app/core/header/notifications/notifications.component.ts
@@ -154,6 +154,7 @@ export class NotificationsComponent implements OnInit {
   renderLastestTx(txMemory: Tx[]) { // Render the last 8 Txs
     const lastestTx: Tx[] = this.getLastestTx(txMemory);
     lastestTx.forEach(c => this.addNewNotification(c));
+    lastestTx.forEach(c => this.setTxFinished(c));
   }
 
   emitCounter() { // Set the notificationsCounter on new Notifications
