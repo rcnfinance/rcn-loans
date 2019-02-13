3e8a68713aa284878a153051539228583738fc83
Ignacio <ignacio@ripiocredit.network>
2019-02-13 11:40:25 -0300

    Punitory Bug --> env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -19,32 +19,32 @@ export const environment = {
   version_name: p.version_name,
   build: getBuild(),
   production: false,
-  url: 'https://mainnet.rcn.loans/',
-  envName: 'main',
+  url: 'https://testnet.rcn.loans/',
+  envName: 'dev',
+  gaTracking: 'UA-122615331-2',
   identity: 'https://20mq9e6amd.execute-api.us-east-2.amazonaws.com/alpha/',
   buyLink: 'https://www.bancor.network/communities/5a92b438583f4a0001f75f42/about',
   version_verbose: p.version + '@' + getBuild() + ' - ' + p.version_name,
   sentry: 'https://7082f6389c9b4d5ab9d7b2cde371da2a@sentry.io/1261533',
-  gaTracking: 'UA-122615331-3',
   rcn_node: {
-    loan: 'https://rnode.rcn.loans/v1/loans/$id/'
+    loan: 'https://testnet.rnode.rcn.loans/v1/loans/$id/'
   },
   network: {
-    id: '1',
-    name: 'Main',
+    id: '3',
+    name: 'Ropsten',
     explorer: {
-      address: 'https://etherscan.io/address/${address}',
-      tx: 'https://etherscan.io/tx/${tx}'
+      address: 'https://ropsten.etherscan.io/address/${address}',
+      tx: 'https://ropsten.etherscan.io/tx/${tx}'
     },
-    provider: 'https://main.node.rcn.loans:8545/'
+    provider: 'https://ropsten.node.rcn.loans:8545/'
   },
   contracts: {
-    rcnToken: '0xf970b8e36e23f7fc3fd752eea86f8be8d83375a6',
-    basaltEngine: '0xba5a17f8ad40dc2c955d95c0547f3e6318bd72e7',
-    engineExtension: '0x3143f397685daa5f48f77c5d3ea4cbe61f294d88',
+    rcnToken: '0x2f45b6fb2f28a73f110400386da31044b2e953d4',
+    basaltEngine: '0xbee217bfe06c6faaa2d5f2e06ebb84c5fb70d9bf',
+    engineExtension: '0x3b86e29fc3e8a626735b0194aef13c6051eb6c84',
     converter: {
-      converterRamp: '0x56783153d0a8ccb009dcb79df5337835ed1a9d6c',
-      tokenConverter: '0x3b81db7c9fe71a2c6d78f9ae2fe4df4c92272622',
+      converterRamp: '0xeaf063101ae319a18330ff78fdd81d992bf83349',
+      tokenConverter: '0xc4b1b3083174716542ef387326e58293917bf3bf',
       ethAddress: '0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
       params: {
         marginSpend: 5000,
@@ -53,21 +53,27 @@ export const environment = {
       }
     },
     decentraland: {
-      mortgageCreator: '0x90263Ea5C57Dc6603CA7202920735A6E31235bB9',
-      mortgageManager: '0x9ABf1295086aFA0E49C60e95c437aa400c5333B8',
-      landMarket: '0xb3bca6f5052c7e24726b44da7403b56a8a1b98f8'
+      mortgageCreator: '0x0e4c24f71c8679b8af8e5a22aac3816e2b23f1cc',
+      mortgageManager: '0x31ebb4ffd5e34acfc87ea21a0c56157188f3f0e1',
+      landMarket: '0x5424912699dabaa5f2998750c1c66e73d67ad219'
     }
   },
-  blacklist: [],
+  blacklist: [
+    {
+      key: 'oracle',
+      forbidden: [
+        '0x0ac18b74b5616fdeaeff809713d07ed1486d0128'
+      ]
+    }
+  ],
   filters: {
-    openLoans: '0xb18aa197fdb7d20c695dac42da71eb55883fb253',
-    nonExpired: '0x56a65418a09aa5cd0cb79d437cb1d318037817d7',
-    validMortgage: '0x7c9ee6f211093351612345fce308cbf86e562b69',
-    lenderIn: '0x5ef16f3412e7c01e5c9803caae1322b28596d0bd',
-    ongoing: '0x3b80f3028af6ab654b6b0188e651667ade313e1b',
-    stub: '0xc7fb7d6fb0c787d5454cbd1b8140ec9624519668'
+    openLoans: '0x3e703de416a62525c8653be11d71486550618ec8',
+    nonExpired: '0xe084b7cf7f6869a96cd72962047bf65e6d55e1e1',
+    validMortgage: '0x93b08a8a3cf148d62c20d3daa0b1bdf813bb7a21',
+    lenderIn: '0xe52eac8af912b8b3196b2921f12b66c91b39e025',
+    ongoing: '0xc247ba1b89af5f2654184f0c5a8e8f1ea48c55e3'
   },
   dir: {
-    '0x263231ed9b51084816a44e18d16c0f6d0727491f': Agent.RipioCreator
+    '0xdc5fdc6d0c24573c7e2ac3896ab10e376be6da86': Agent.RipioCreator
   }
 };
