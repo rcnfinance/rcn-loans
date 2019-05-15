8594e8b65bb3f08e4451de640621957845b0af86
Nicolas <menendeznm@gmail.com>
2019-05-15 11:29:44 -0300

    change env-dev contracts config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -46,8 +46,8 @@ export const environment = {
     basaltEngine: '0xbee217bfe06c6faaa2d5f2e06ebb84c5fb70d9bf',
     engineExtension: '0x3b86e29fc3e8a626735b0194aef13c6051eb6c84',
     diaspore: {
-      debtEngine: '0xe4BfBBB04844cdEbd6b7814183f92E0703257d48',
-      loanManager: '0x978ef6D2bd7559181e6Ac82fFa5C875364d9071b',
+      debtEngine: '0xc1b4f53ab978de0bbc347c6d85cef40226f85758',
+      loanManager: '0x6b3e82ddc85710ed9c32c61b317ade1b2a80fc77',
       viewRequets: '0x7edb5117f91514579e3c8d39eed71e6be278632a',
       filters: {
         debtCreator: '0x998a67ce5827cb372fe07942561006c7a76cf06f',
