3e901aa8f72dc74cbd1449c744c835ba9a0a9220
camila <camidlserna@hotmail.com>
2019-05-20 17:47:57 -0300

    Re-setting country service

--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -22,8 +22,7 @@ export class CountriesService {
   }
 
   async lendEnabled(): Promise<Boolean> {
-    // const country = await this.country;
-    // return country.toUpperCase() !== 'US';
-    return false;
+    const country = await this.country;
+    return country.toUpperCase() !== 'US';
   }
 }
