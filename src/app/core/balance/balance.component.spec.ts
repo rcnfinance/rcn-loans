74e61f5b372dae1e7efc709cf162151283232ebb
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-07-15 14:52:32 -0300

    add expect to balance test

--- src/app/core/balance/balance.component.spec.ts
+++ src/app/core/balance/balance.component.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '../../shared/shared.module';
 import { MaterialModule } from '../../material/material.module';
 import { BalanceComponent } from './balance.component';
+import { readComponent } from '../../utils/utils.test';
 
 describe('BalanceComponent', () => {
   let component: BalanceComponent;
@@ -29,4 +30,34 @@ describe('BalanceComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display balance', async () => {
+    let thisComponent: any = component;
+
+    // set balance amount
+    thisComponent.rcnBalance = 25000.789;
+    thisComponent.rcnAvailable = 3000;
+    thisComponent.basaltLoansWithBalance = ['743'];
+
+    // update template
+    await thisComponent.updateDisplay();
+    fixture.detectChanges();
+
+    // logic expect
+    expect(thisComponent.displayBalance).toBe('25000.8');
+    expect(thisComponent.displayAvailable).toBe('3000');
+    expect(thisComponent.canWithdraw).toBeTruthy();
+
+    // ui expect
+    const withdrawPayments = readComponent(fixture, '.pending');
+    expect(withdrawPayments).toBeDefined();
+
+    const displayAvailable = Number(withdrawPayments.innerText.replace('add_circle', ''));
+    expect(displayAvailable).toBe(3000);
+  });
+
+  it('should hide withdraw button', async(() => {
+    const withdrawPayments = readComponent(fixture, '.pending');
+    expect(withdrawPayments).toBeUndefined();
+  }));
 });
