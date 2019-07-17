1b5ec29ee6ed89cba78093122f3fcbc821bb989c
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-07-17 15:34:20 -0300

    fix circle progress component unit test

--- src/app/views/create-loan/circle-progress/circle-progress.component.spec.ts
+++ src/app/views/create-loan/circle-progress/circle-progress.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CircleProgressComponent } from './circle-progress.component';
 
 describe('CircleProgressComponent', () => {
@@ -8,7 +8,8 @@ describe('CircleProgressComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CircleProgressComponent ]
+      declarations: [ CircleProgressComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
