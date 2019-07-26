a09559bec8c2d6b99b169bc416188f2cb2880675
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-07-26 18:00:44 -0300

    add pmt util method

--- src/app/utils/utils.spec.ts
+++ src/app/utils/utils.spec.ts
@@ -5,4 +5,12 @@ describe('Utils', () => {
     const interestRate = (annualPercentage) => Utils.toInterestRate(annualPercentage);
     expect(interestRate(25)).toBe(12441600000000);
   });
+
+  it('should return payment based on payments and interest rate', () => {
+    const amount = 3000;
+    const installments = 6;
+    const rate = 0.5;
+    const installmentDuration = 15 / 360;
+    expect(-Utils.pmt(installmentDuration * rate, installments, amount).toFixed(2)).toBe(537.08);
+  });
 });
