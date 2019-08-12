c4bfeed360d2a9b1a923082512fbe0d1f5c9f66f
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-08-12 11:42:21 -0300

    import material module in the collateral unit test

--- src/app/views/loan-detail/detail-collateral/detail-collateral.component.spec.ts
+++ src/app/views/loan-detail/detail-collateral/detail-collateral.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { APP_BASE_HREF } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MaterialModule } from '../../../material/material.module';
 import { DetailCollateralComponent } from './detail-collateral.component';
 
 describe('DetailCollateralComponent', () => {
@@ -9,6 +10,9 @@ describe('DetailCollateralComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MaterialModule
+      ],
       declarations: [
         DetailCollateralComponent
       ],
