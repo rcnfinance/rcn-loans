0dbfb7b677556d982ab75b8b97a6eb27134256d9
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-08-14 14:29:33 -0300

    update api endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -30,7 +30,7 @@ export const environment = {
     loan: 'https://ropsten-rnode.rcn.loans/v1/commits?id_loan=$id'
   },
   rcn_node_api: {
-    url: 'https://diaspore-ropsten-rnode.rcn.loans/v4/'
+    url: 'http://diaspore-ropsten.diaspore.18.221.166.157.xip.io/v4/'
   },
   network: {
     id: '3',
