e274095184f842b73c041196ec9777562f82c8db
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-08-23 16:31:53 -0300

    collateral dialog dynamic data

--- src/app/shared/collateral-add-form/collateral-add-form.component.ts
+++ src/app/shared/collateral-add-form/collateral-add-form.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Utils } from '../../utils/utils';
+// App models
+import { Loan } from '../../models/loan.model';
+import { Collateral } from '../../models/collateral.model';
+// App services
+import { Web3Service } from '../../services/web3.service';
+import { CurrenciesService } from '../../services/currencies.service';
 
 @Component({
   selector: 'app-collateral-add-form',
@@ -7,14 +14,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./collateral-add-form.component.scss']
 })
 export class CollateralAddFormComponent implements OnInit {
+  @Input() loan: Loan;
+  @Input() collateral: Collateral;
   @Output() submitAdd = new EventEmitter<number>();
 
   form: FormGroup;
+  collateralAmount: string;
+  collateralAsset: string;
+  shortAccount: string;
 
-  constructor() {}
+  constructor(
+    private web3Service: Web3Service,
+    private currenciesService: CurrenciesService
+  ) { }
 
   ngOnInit() {
     this.createFormControls();
+    this.getCollateralDetails();
+    this.getAccount();
   }
 
   /**
@@ -27,6 +44,28 @@ export class CollateralAddFormComponent implements OnInit {
         Validators.min(0)
       ])
     });
+  }
+
+  /**
+   * Get collateral parsed data
+   */
+  getCollateralDetails() {
+    const web3: any = this.web3Service.web3;
+    const collateral: Collateral = this.collateral;
+    const collateralCurrency = this.currenciesService.getCurrencyByKey('address', collateral.token);
+    this.collateralAmount = Utils.formatAmount(web3.fromWei(collateral.amount));
+    this.collateralAsset = collateralCurrency.symbol;
+  }
+
+  /**
+   * Get short account address
+   */
+  async getAccount() {
+    const web3: any = this.web3Service.web3;
+    let account = await this.web3Service.getAccount();
+    account = web3.toChecksumAddress(account);
+
+    this.shortAccount = Utils.shortAddress(account);
   }
 
   onSubmit(form: FormGroup) {
