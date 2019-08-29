1c96e3ff77b213aa26e738d1a55caacf23dce6c7
Camila <camila@Camilas-MacBook-Pro.local>
2019-08-29 17:34:59 -0300

    Setting RouteModule

--- src/app/views/address/address.module.ts
+++ src/app/views/address/address.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 // App Modules
 import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 import { SharedModule } from '../../shared/shared.module';
 import { MaterialModule } from './../../material/material.module';
+// import { AppRoutingModule } from '../../app-routing/app-routing.module';
 // App Services
 import { ContractsService } from './../../services/contracts.service';
 import { AvailableLoansService } from '../../services/available-loans.service';
@@ -15,7 +17,8 @@ import { AddressComponent } from './address.component';
     CommonModule,
     NgxSpinnerModule,
     MaterialModule,
-    SharedModule
+    SharedModule,
+    // AppRoutingModule
   ],
   declarations: [
     AddressComponent
