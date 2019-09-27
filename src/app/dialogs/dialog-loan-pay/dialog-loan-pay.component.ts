ef5fafe5abb13e76c60539a8864116446891348e
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-09-27 12:54:08 -0300

    mockup dialog loan pay

--- src/app/dialogs/dialog-loan-pay/dialog-loan-pay.component.ts
+++ src/app/dialogs/dialog-loan-pay/dialog-loan-pay.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material';
 import { Loan } from '../../models/loan.model';
+import { Utils } from '../../utils/utils';
+
+import { Web3Service } from './../../services/web3.service';
 
 @Component({
   selector: 'app-dialog-loan-pay',
@@ -11,17 +15,61 @@ export class DialogLoanPayComponent implements OnInit {
 
   @Input() loan: Loan;
 
+  loading: boolean;
+  account: string;
+  shortAccount: string;
+
   constructor(
-    public dialogRef: MatDialogRef<any>
+    public dialogRef: MatDialogRef<any>,
+    private web3Service: Web3Service
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.dialogRef.updateSize('auto', 'auto');
+    await this.loadAccount();
   }
 
-  submit(amount: any) {
-    // If pressed Cancel, amount is false
+  /**
+   * Set account address
+   */
+  async loadAccount() {
+    const web3: any = this.web3Service.web3;
+    const account = await this.web3Service.getAccount();
+
+    this.account = web3.toChecksumAddress(account);
+    this.shortAccount = Utils.shortAddress(this.account);
+  }
+
+  /**
+   * Emitted when form is submitted
+   * @param form Form group
+   * @fires submitAdd
+   */
+  onSubmit(form: FormGroup) {
+    const amount = form.value.amount;
+
+    if (this.loading) {
+      return;
+    }
+    // if (collateralRatio < balanceRatio) {
+    //   this.showMessage(`The collateral is too low, make sure it is greater than ${ balanceRatio }%`);
+    //   return;
+    // }
+    // if (amount <= 0) {
+    //   this.showMessage(`The collateral amount must be greater than 0`);
+    //   return;
+    // }
     this.dialogRef.close(amount);
   }
 
+  /**
+   * Get submit button text
+   * @return Button text
+   */
+  get submitButtonText(): string {
+    if (!this.loading) {
+      return 'Pay';
+    }
+    return 'Paying...';
+  }
 }
