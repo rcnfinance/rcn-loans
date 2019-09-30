06d51351070a79d90db345bce26755f53ae0f2d0
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-09-30 19:17:50 -0300

    dynamic values in the pay dialog

--- src/app/dialogs/dialog-loan-pay/dialog-loan-pay.component.ts
+++ src/app/dialogs/dialog-loan-pay/dialog-loan-pay.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { MatDialogRef } from '@angular/material';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Loan } from '../../models/loan.model';
 import { Utils } from '../../utils/utils';
 
@@ -13,20 +13,36 @@ import { Web3Service } from './../../services/web3.service';
 })
 export class DialogLoanPayComponent implements OnInit {
 
-  @Input() loan: Loan;
-
+  loan: Loan;
   loading: boolean;
+  form: FormGroup;
+
   account: string;
   shortAccount: string;
+  pendingAmount: string;
+  currency: any;
 
   constructor(
     public dialogRef: MatDialogRef<any>,
-    private web3Service: Web3Service
-  ) { }
+    private web3Service: Web3Service,
+    @Inject(MAT_DIALOG_DATA) public data
+  ) {
+    this.loan = this.data.loan;
+  }
 
   async ngOnInit() {
     this.dialogRef.updateSize('auto', 'auto');
+    this.buildForm();
+    this.loadDetail();
     await this.loadAccount();
+  }
+
+  buildForm() {
+    this.form = new FormGroup({
+      amount: new FormControl(null, [
+        Validators.required
+      ])
+    });
   }
 
   /**
@@ -41,24 +57,36 @@ export class DialogLoanPayComponent implements OnInit {
   }
 
   /**
-   * Emitted when form is submitted
+   * Loan payment details
+   */
+  loadDetail() {
+    const currency = this.loan.currency;
+    const pendingAmount = currency.fromUnit(this.loan.debt.model.estimatedObligation);
+
+    this.currency = currency;
+    this.pendingAmount = Utils.formatAmount(pendingAmount);
+    this.form.controls.amount.setValidators([
+      Validators.required,
+      Validators.max(pendingAmount)
+    ]);
+  }
+
+  /**
+   * Submit form
    * @param form Form group
    * @fires submitAdd
    */
-  onSubmit(form: FormGroup) {
+  submitForm() {
+    const form: FormGroup = this.form;
     const amount = form.value.amount;
 
+    if (!form.valid) {
+      // TODO: Ux tip - show snackbar with error description
+      return;
+    }
     if (this.loading) {
       return;
     }
-    // if (collateralRatio < balanceRatio) {
-    //   this.showMessage(`The collateral is too low, make sure it is greater than ${ balanceRatio }%`);
-    //   return;
-    // }
-    // if (amount <= 0) {
-    //   this.showMessage(`The collateral amount must be greater than 0`);
-    //   return;
-    // }
     this.dialogRef.close(amount);
   }
 
