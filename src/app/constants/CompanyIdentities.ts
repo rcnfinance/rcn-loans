b0e6904af54c8b46b5e49c1530117977cba6db58
Camila <camila@Camilas-MacBook-Pro.local>
2019-10-01 16:44:06 -0300

    Adding ripio logo to loan detail's header

--- src/app/constants/CompanyIdentities.ts
+++ src/app/constants/CompanyIdentities.ts
@@ -8,8 +8,8 @@ export const companyIdentities = {
     'Ripio',
     '--',
     'Ripio is now one of the main Blockchain companies in Latin America. Ripio main product is their mobile wallet that operates on Bitcoin, Ether and local currency: users can receive, store, buy / sell and send cryptocurrency, and also make digital payments from their mobile phones, and also request micro-loans to finance their purchases.',
-    'https://ripio-cms-us.s3.amazonaws.com/filer_public/80/d7/80d76109-a560-446c-9385-d6d911168dbe/logo-ripio-white.svg',
     './assets/logos/logo-ripio-white.svg',
+    './assets/logos/logo-ripio-white-brand.svg',
     [
       {
         icon: 'fas fa-calendar-alt',
