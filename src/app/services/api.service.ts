3f9f9c6b03c571f95e1d3788aa44ea089670c994
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-10-11 16:58:03 -0300

    add support for basalt api in the http service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -12,26 +12,37 @@ import { Web3Service } from './web3.service';
 export class ApiService {
 
   installmentModelAddress = '0x2B1d585520634b4c7aAbD54D73D34333FfFe5c53';
-  url = environment.rcn_node_api.url;
+  diasporeUrl = environment.rcn_node_api.diasporeUrl;
+  basaltUrl = environment.rcn_node_api.basaltUrl;
 
   constructor(
     private http: HttpClient,
     private web3Service: Web3Service
   ) { }
 
-  // Loads all loans lent by the account that is logged in
-  async getLoansOfLender(lender: string): Promise<Loan[]> {
+  /**
+   * Get all loans lent by the account that is logged in
+   * @param lender Lender address
+   * @param network Selected network
+   * @return Loans array
+   */
+  async getLoansOfLender(
+    lender: string,
+    network: Network
+  ): Promise<Loan[]> {
     const web3 = this.web3Service.web3;
+    const apiUrl: string = this.getApiUrl(network);
+
     let allLoansOfLender: Loan[] = [];
     let apiCalls = 0;
     let page = 0;
     try {
       lender = web3.toChecksumAddress(lender);
-      const data: any = await this.http.get(this.url.concat('loans?open=false&page=' + page + '&lender=' + lender)).toPromise();
+      const data: any = await this.http.get(apiUrl.concat('loans?open=false&page=' + page + '&lender=' + lender)).toPromise();
       if (page === 0) {
         apiCalls = Math.ceil(data.meta.resource_count / data.meta.page_size);
       }
-      const activeLoans = await this.getAllCompleteLoans(data.content);
+      const activeLoans = await this.getAllCompleteLoans(data.content, network);
       allLoansOfLender = allLoansOfLender.concat(activeLoans);
       page++;
     } catch (err) {
@@ -40,12 +51,12 @@ export class ApiService {
 
     const urls = [];
     for (page; page < apiCalls; page++) {
-      const url = this.url.concat('loans?open=false&page=' + page + '&lender=' + lender);
+      const url = apiUrl.concat('loans?open=false&page=' + page + '&lender=' + lender);
       urls.push(url);
     }
     const responses = await this.getAllUrls(urls);
 
-    const allApiLoans = await this.getAllApiLoans(responses);
+    const allApiLoans = await this.getAllApiLoans(responses, network);
 
     for (const apiLoans of allApiLoans) {
       allLoansOfLender = allLoansOfLender.concat(apiLoans);
@@ -54,18 +65,23 @@ export class ApiService {
     return allLoansOfLender;
   }
 
-  // Gets all loans that were lent and there status is ongoing. Meaning that they are not canceled or finished.
-  async getActiveLoans(): Promise<Loan[]> {
+  /**
+   * Gets all loans that were lent and there status is ongoing. Meaning that they are not canceled or finished.
+   * @param network Selected network
+   * @return Loans array
+   */
+  async getActiveLoans(network: Network): Promise<Loan[]> {
+    const apiUrl: string = this.getApiUrl(network);
     let allActiveLoans: Loan[] = [];
     let apiCalls = 0;
     let page = 0;
     try {
 
-      const data: any = await this.http.get(this.url.concat('loans?open=false&page=' + page)).toPromise();
+      const data: any = await this.http.get(apiUrl.concat('loans?open=false&page=' + page)).toPromise();
       if (page === 0) {
         apiCalls = Math.ceil(data.meta.resource_count / data.meta.page_size);
       }
-      const activeLoans = await this.getAllCompleteLoans(data.content);
+      const activeLoans = await this.getAllCompleteLoans(data.content, network);
       allActiveLoans = allActiveLoans.concat(activeLoans);
       page++;
 
@@ -75,12 +91,12 @@ export class ApiService {
 
     const urls = [];
     for (page; page < apiCalls; page++) {
-      const url = this.url.concat('loans?open=false&page=' + page);
+      const url = apiUrl.concat('loans?open=false&page=' + page);
       urls.push(url);
     }
     const responses = await this.getAllUrls(urls);
 
-    const allApiLoans = await this.getAllApiLoans(responses);
+    const allApiLoans = await this.getAllApiLoans(responses, network);
 
     for (const apiLoans of allApiLoans) {
       allActiveLoans = allActiveLoans.concat(apiLoans);
@@ -89,9 +105,18 @@ export class ApiService {
     return allActiveLoans;
   }
 
-  async getLoan(id: string): Promise<Loan> {
-    const data: any = await this.http.get(this.url.concat(`loans/${id}`)).toPromise();
-    const loan = await this.completeLoanModels(data.content);
+  /**
+   * Gets loan by ID
+   * @param network Selected network
+   * @return Loan
+   */
+  async getLoan(
+    id: string,
+    network: Network
+  ): Promise<Loan> {
+    const apiUrl: string = this.getApiUrl(network);
+    const data: any = await this.http.get(apiUrl.concat(`loans/${id}`)).toPromise();
+    const loan = await this.completeLoanModels(data.content, network);
     try {
       return loan;
     } catch {
@@ -116,13 +141,13 @@ export class ApiService {
     }
   }
 
-  async getAllCompleteLoans(apiLoans: any[]) {
+  async getAllCompleteLoans(apiLoans: any[], network: Network) {
     try {
       const loans = await Promise.all(
         apiLoans.map(
-          loan =>
-            this.completeLoanModels(loan)));
-
+          loan => this.completeLoanModels(loan, network)
+        )
+      );
       return (loans);
 
     } catch (error) {
@@ -131,11 +156,11 @@ export class ApiService {
     }
   }
 
-  async getAllApiLoans(responses: any[]) {
+  async getAllApiLoans(responses: any[], network: Network) {
     try {
       const activeLoans = await Promise.all(
         responses.map(
-          response => this.getAllCompleteLoans(response.content)
+          response => this.getAllCompleteLoans(response.content, network)
         ));
 
       return (activeLoans);
@@ -146,20 +171,34 @@ export class ApiService {
     }
   }
 
-  // Get all loans request that are open, not canceled or expired.
-  async getRequests(now: number): Promise<Loan[]> {
+  /**
+   * Get all loans request that are open, not canceled or expired.
+   * @param now Timestamp
+   * @param network Selected network
+   * @return Loan
+   */
+  async getRequests(
+    now: number,
+    network: Network
+  ): Promise<Loan[]> {
+    const apiUrl: string = this.getApiUrl(network);
+    const filterExpiration: string = this.getApiFilterKey('expiration', network);
     let allRequestLoans: Loan[] = [];
     let apiCalls = 0;
     let page = 0;
-    try {
 
-      const data: any = await this.http.get(this.url.concat('loans?open=true&canceled=false&approved=true&page=' + page
-        + '&expiration__gt=' + now)).toPromise();
+    try {
+      const data: any = await this.http.get(apiUrl.concat(
+        `loans?open=true&canceled=false&approved=true&page=${ page }&${ filterExpiration }=${ now }`
+      )).toPromise();
+      // FIXME: hardcoded
+      if (network === Network.Basalt) { data.meta.resource_count = 700; }
+
       if (page === 0) {
         apiCalls = Math.ceil(data.meta.resource_count / data.meta.page_size);
       }
 
-      const loansRequests = await this.getAllCompleteLoans(data.content);
+      const loansRequests = await this.getAllCompleteLoans(data.content, network);
       const filteredLoans = loansRequests
         .filter(loan =>
           loan.model !== this.installmentModelAddress &&
@@ -174,14 +213,15 @@ export class ApiService {
 
     const urls = [];
     for (page; page < apiCalls; page++) {
-      const url = this.url.concat('loans?open=true&canceled=false&approved=true&page=' + page
-        + '&expiration__gt=' + now);
+      const url = apiUrl.concat(
+        `loans?open=true&canceled=false&approved=true&page=${ page }&${ filterExpiration }=${ now }`
+      );
       urls.push(url);
     }
     const responses = await this.getAllUrls(urls);
 
     for (const response of responses) {
-      const loansRequests = await this.getAllCompleteLoans(response.content);
+      const loansRequests = await this.getAllCompleteLoans(response.content, network);
       const notExpiredResquestLoans = loansRequests.filter(loan => loan.model !== this.installmentModelAddress);
       allRequestLoans = allRequestLoans.concat(notExpiredResquestLoans);
     }
@@ -189,80 +229,137 @@ export class ApiService {
     return allRequestLoans;
   }
 
-  async completeLoanModels(loan: any): Promise<Loan> {
-
+  async completeLoanModels(
+    loan: any,
+    network: Network
+  ): Promise<Loan> {
+    const apiUrl: string = this.getApiUrl(network);
     const engine = environment.contracts.diaspore.loanManager;
 
     let oracle: Oracle;
     if (loan.oracle !== Utils.address0x) {
       const currency = loan.currency ? Utils.hexToAscii(loan.currency.replace(/^[0x]+|[0]+$/g, '')) : '';
       oracle = new Oracle(
-        Network.Diaspore,
+        network,
         loan.oracle,
         currency,
         loan.currency
       );
     } else {
       oracle = new Oracle(
-        Network.Diaspore,
+        network,
         loan.oracle,
         'RCN',
         loan.currency
       );
     }
 
-    const descriptor = new Descriptor(
-      Network.Diaspore, Number(loan.descriptor.first_obligation), Number(loan.descriptor.total_obligation),
-      Number(loan.descriptor.duration), Number(loan.descriptor.interest_rate),
-      LoanUtils.decodeInterest(Number(loan.descriptor.punitive_interest_rate)),
-      Number(loan.descriptor.frequency), Number(loan.descriptor.installments));
-
+    let descriptor: Descriptor;
     let debt: Debt;
-    if (!loan.open && !loan.canceled && loan.status) {
-      const data: any = await this.http.get(this.url.concat(`model_debt_info/${loan.id}`)).toPromise();
-      const paid = data.paid;
-      const dueTime = data.due_time;
-      const estimatedObligation = data.estimated_obligation;
-      const nextObligation = data.next_obligation;
-      const currentObligation = data.current_obligation;
-      const debtBalance = data.debt_balance;
-      const owner = data.owner;
-      debt = new Debt(
-        Network.Diaspore,
-        loan.id,
-        new Model(
+
+    switch (network) {
+      case Network.Basalt:
+        return LoanUtils.createBasaltLoan(loan);
+
+      case Network.Diaspore:
+        descriptor = new Descriptor(
           Network.Diaspore,
+          Number(loan.descriptor.first_obligation),
+          Number(loan.descriptor.total_obligation),
+          Number(loan.descriptor.duration),
+          Number(loan.descriptor.interest_rate),
+          LoanUtils.decodeInterest(
+            Number(loan.descriptor.punitive_interest_rate)
+          ),
+          Number(loan.descriptor.frequency),
+          Number(loan.descriptor.installments)
+        );
+
+        // set debt model
+        if (!loan.open && !loan.canceled && loan.status) {
+          const data: any = await this.http.get(apiUrl.concat(`model_debt_info/${loan.id}`)).toPromise();
+          const paid = data.paid;
+          const dueTime = data.due_time;
+          const estimatedObligation = data.estimated_obligation;
+          const nextObligation = data.next_obligation;
+          const currentObligation = data.current_obligation;
+          const debtBalance = data.debt_balance;
+          const owner = data.owner;
+          debt = new Debt(
+            Network.Diaspore,
+            loan.id,
+            new Model(
+              Network.Diaspore,
+              loan.model,
+              paid,
+              nextObligation,
+              currentObligation,
+              estimatedObligation,
+              dueTime
+            ),
+            debtBalance,
+            engine,
+            owner,
+            oracle
+          );
+        }
+        const status = loan.canceled ? Status.Destroyed : Number(loan.status);
+
+        return new Loan(
+          network,
+          loan.id,
+          engine,
+          Number(loan.amount),
+          oracle,
+          descriptor,
+          loan.borrower,
+          loan.creator,
+          status,
+          Number(loan.expiration),
           loan.model,
-          paid,
-          nextObligation,
-          currentObligation,
-          estimatedObligation,
-          dueTime
-        ),
-        debtBalance,
-        engine,
-        owner,
-        oracle
-      );
+          loan.cosigner,
+          debt
+        );
+
+      default:
+        break;
     }
-
-    const status = loan.canceled ? Status.Destroyed : Number(loan.status);
-
-    const newLoan = new Loan(
-      Network.Diaspore,
-      loan.id,
-      engine,
-      Number(loan.amount),
-      oracle,
-      descriptor,
-      loan.borrower,
-      loan.creator,
-      status,
-      Number(loan.expiration),
-      loan.model,
-      loan.cosigner,
-      debt
-    );
-    return newLoan;
   }
+
+  /**
+   * Return the api url according to the chosen network
+   * @param network Selected network
+   * @return Api url
+   */
+  private getApiUrl(network: Network) {
+    switch (network) {
+      case Network.Basalt:
+        return this.basaltUrl;
+
+      case Network.Diaspore:
+        return this.diasporeUrl;
+
+      default:
+        break;
+    }
+  }
+
+  /**
+   * Return the api filter key according to the chosen network
+   * @param network Selected network
+   * @return Api filter key
+   */
+  private getApiFilterKey(filter: string, network: Network) {
+    const filters = {
+      [Network.Diaspore]: {
+        expiration: 'expiration__gt'
+      },
+      [Network.Basalt]: {
+        expiration: 'expiration_requests__gt'
+      }
+    };
+
+    return filters[network][filter];
+  }
+
 }
