f0976564b1c030252188e5b6957d30d471500570
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-10-26 23:41:15 -0300

    refactor footer component

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -16,12 +16,14 @@ import { AvailableLoansService } from '../../services/available-loans.service';
 })
 export class FooterComponent implements OnInit, OnDestroy {
   account: string;
-  version: string;
-  versionString: string;
-  linkContract: string;
-  contract: string;
   title: string;
   available: any;
+  socialIcons: {
+    url: string;
+    label: string;
+    class: string;
+    text?: string;
+  }[];
 
   // Nav Mobile toggled
   navmobileToggled = false;
@@ -36,18 +38,66 @@ export class FooterComponent implements OnInit, OnDestroy {
 
   constructor(
     public dialog: MatDialog,
+    private titleService: TitleService,
     private web3Service: Web3Service,
     private sidebarService: SidebarService,
-    public titleService: TitleService,
     private availableLoansService: AvailableLoansService
   ) {}
 
   async ngOnInit() {
     const env = environment;
-    this.contract = env.contracts.basaltEngine;
-    this.version = env.version;
-    this.versionString = env.version + '-' + env.build + ' - ' + env.version_name;
-    this.linkContract = env.network.explorer.address.replace('${address}', env.contracts.basaltEngine);
+    const contract = env.contracts.basaltEngine;
+    const linkContract = env.network.explorer.address.replace('${address}', env.contracts.basaltEngine);
+    const version = env.version;
+    const versionString = env.version + '-' + env.build + ' - ' + env.version_name;
+    this.socialIcons = [
+      {
+        url: linkContract,
+        label: `Basalt engine contract ${ contract }`,
+        class: 'fas fa-link'
+      },
+      {
+        url: 'https://github.com/ripio/rcn-loans/releases',
+        label: `Version ${ versionString }`,
+        class: 'fas fa-code-branch ml-2',
+        text: version
+      },
+      {
+        url: 'https://www.facebook.com/ripiocredit.network/',
+        label: 'Facebook',
+        class: 'fab fa-facebook-f'
+      },
+      {
+        url: 'https://twitter.com/rcn_token?lang=en',
+        label: 'Twitter',
+        class: 'fab fa-twitter'
+      },
+      {
+        url: 'https://t.me/RCNtalk',
+        label: 'Telegram',
+        class: 'fab fa-telegram-plane'
+      },
+      {
+        url: 'https://www.linkedin.com/company/ripio-credit-network/',
+        label: 'Linkedin',
+        class: 'fab fa-linkedin-in'
+      },
+      {
+        url: 'https://www.reddit.com/r/rcn_token/',
+        label: 'Reddit',
+        class: 'fab fa-reddit'
+      },
+      {
+        url: 'https://github.com/ripio/rcn.loans',
+        label: 'Github',
+        class: 'fab fa-github'
+      },
+      {
+        url: 'https://discord.gg/62U  CxbS',
+        label: 'Discord',
+        class: 'fab fa-discord'
+      }
+    ];
 
     // Service subscriber
     this.subscriptions.sidebar = this.sidebarService.currentNavmobile.subscribe(
