a15222edd22c0c33daa0d0477f49ad04aecb462b
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-11-04 18:04:15 -0300

    dynamic create loan card component

--- src/app/views/create-loan/create-loan-card/create-loan-card.component.ts
+++ src/app/views/create-loan/create-loan-card/create-loan-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { Utils } from './../../../utils/utils';
 import { Loan } from './../../../models/loan.model';
 
 @Component({
@@ -6,13 +7,145 @@ import { Loan } from './../../../models/loan.model';
   templateUrl: './create-loan-card.component.html',
   styleUrls: ['./create-loan-card.component.scss']
 })
-export class CreateLoanCardComponent implements OnInit {
+export class CreateLoanCardComponent implements OnInit, OnChanges {
 
   @Input() loan: Loan;
   expanded: boolean;
 
+  amount: string;
+  expectedReturn: string;
+  annualInterest: string;
+  installments: number;
+  durationDate: string;
+  expirationDate: string;
+  paysDetail = [];
+
   constructor() { }
 
   ngOnInit() { }
+
+  ngOnChanges(changes) {
+    const { loan } = changes;
+
+    if (loan && loan.currentValue) {
+      this.loadDetail();
+    }
+  }
+
+  /**
+   * Load loan details
+   */
+  loadDetail() {
+    const loan: Loan = this.loan;
+
+    const amount = loan.currency.fromUnit(loan.amount);
+    this.amount = amount ? Utils.formatAmount(amount) : null;
+
+    if (loan.descriptor) {
+      const duration = loan.descriptor.duration;
+      const expiration = loan.expiration;
+      this.installments = loan.descriptor.installments;
+      this.durationDate = duration ? Utils.formatDelta(duration, 2) : null;
+      this.expirationDate = expiration ? Utils.formatDelta(expiration, 2) : null;
+      this.annualInterest = Number(loan.descriptor.interestRate).toString();
+      this.expectedReturn = this.calculateExpectedReturn();
+      return;
+    }
+
+    this.installments = 1;
+    this.annualInterest = null;
+    this.durationDate = null;
+    this.expirationDate = null;
+  }
+
+  /**
+   * Calculate the return amount
+   * @return Expected return
+   */
+  calculateExpectedReturn() {
+    const loan: Loan = this.loan;
+    const installments: number = loan.descriptor.installments;
+    const installmentAmount: any = this.expectedInstallmentAmount();
+    const expectedReturn: number = installmentAmount * installments;
+
+    return Utils.formatAmount(expectedReturn);
+  }
+
+  /**
+   * Calculate the installment amount
+   * @return Installment amount
+   */
+  private expectedInstallmentAmount() {
+    const loan: Loan = this.loan;
+    const loanAmount: number = loan.currency.fromUnit(loan.amount);
+    let installmentAmount: number;
+
+    if (loan.descriptor.installments === 1) {
+      const secondsInYear = 86400;
+      const daysInYear = 360;
+      const interest: number = loan.descriptor.interestRate;
+      const annualInterest: number = (interest * loanAmount) / 100;
+      const durationInDays: number = loan.descriptor.duration / secondsInYear;
+      const returnInterest: number = (durationInDays * annualInterest) / daysInYear;
+      installmentAmount = loanAmount + returnInterest;
+    } else {
+      const rate: number = loan.descriptor.interestRate / 100;
+      const installmentDuration: number = loan.descriptor.frequency / 360;
+      installmentAmount = - Utils.pmt(
+        installmentDuration * rate,
+        loan.descriptor.installments,
+        loanAmount,
+        0
+      );
+      this.updateInstallmentsDetails();
+    }
+
+    if (!installmentAmount) {
+      return 0;
+    }
+
+    return Utils.formatAmount(installmentAmount);
+  }
+
+  /**
+   * Fills out installment's details
+   * @return Pay details
+   */
+  private updateInstallmentsDetails() {
+    const paysDetail = [];
+    const loan: Loan = this.loan;
+    const installments = loan.descriptor.installments;
+    const paymentAmount = loan.descriptor.firstObligation;
+
+    for (let i = 0; i < installments; i++) {
+      const pay = i + 1;
+      let payLabel: string;
+
+      switch (Number(pay)) {
+        case 1:
+          payLabel = 'st';
+          break;
+        case 2:
+          payLabel = 'nd';
+          break;
+        default:
+          payLabel = 'th';
+          break;
+      }
+
+      const time = pay * 15;
+      const amount = Utils.formatAmount(loan.currency.fromUnit(paymentAmount));
+      const currency = loan.currency.toString();
+      const payData = {
+        pay: pay + payLabel,
+        time: `${ time } days`,
+        amount: `${ amount } ${ currency }`
+      };
+      paysDetail.push(payData);
+    }
+
+    this.paysDetail = paysDetail;
+    return paysDetail;
+  }
 
 }
