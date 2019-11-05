1e2832968d45992a4aa34f779ed943ce2c8f008c
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-11-05 16:38:19 -0300

    update borrow fab unit test

--- src/app/shared/borrow-fab-button/borrow-fab-button.component.spec.ts
+++ src/app/shared/borrow-fab-button/borrow-fab-button.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from './../../shared/shared.module';
 import { BorrowFabButtonComponent } from './borrow-fab-button.component';
 
 describe('BorrowFabButtonComponent', () => {
@@ -8,7 +10,11 @@ describe('BorrowFabButtonComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BorrowFabButtonComponent ]
+      imports: [
+        RouterTestingModule,
+        SharedModule
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
