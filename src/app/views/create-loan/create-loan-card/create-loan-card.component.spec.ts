d588ae5629e7f9fb0ff582d8e44f39e362800fcf
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-11-08 18:10:27 -0300

    add unit test required module

--- src/app/views/create-loan/create-loan-card/create-loan-card.component.spec.ts
+++ src/app/views/create-loan/create-loan-card/create-loan-card.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from '../../../shared/shared.module';
 import { CreateLoanCardComponent } from './create-loan-card.component';
 
 describe('CreateLoanCardComponent', () => {
@@ -8,6 +10,10 @@ describe('CreateLoanCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+        SharedModule
+      ],
       declarations: [ CreateLoanCardComponent ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
