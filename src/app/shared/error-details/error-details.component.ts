f100f48769d1a9208cc8f6c9800bceb7701dee30
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-11-13 11:19:33 -0300

    add reusable error details component

---
+++ src/app/shared/error-details/error-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+interface ErrorButton {
+  text: string;
+  url: string;
+  class: string;
+}
+
+@Component({
+  selector: 'app-error-details',
+  templateUrl: './error-details.component.html',
+  styleUrls: ['./error-details.component.scss']
+})
+export class ErrorDetailsComponent implements OnInit {
+
+  @Input() label: string;
+  @Input() title: string;
+  @Input() description: string;
+  @Input() image: string;
+  @Input() buttons: ErrorButton[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
