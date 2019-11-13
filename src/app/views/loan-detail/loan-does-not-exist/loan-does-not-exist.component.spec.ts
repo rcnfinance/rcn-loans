ca918d21d280effefc3f79f2b431925004cb2010
Nicolás Acosta <nadesarrolloweb@gmail.com>
2019-11-13 12:21:52 -0300

    update loan does not exist unit test

--- src/app/views/loan-detail/loan-does-not-exist/loan-does-not-exist.component.spec.ts
+++ src/app/views/loan-detail/loan-does-not-exist/loan-does-not-exist.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { LoanDoesNotExistComponent } from './loan-does-not-exist.component';
 
 describe('LoanDoesNotExistComponent', () => {
@@ -8,7 +8,8 @@ describe('LoanDoesNotExistComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoanDoesNotExistComponent ]
+      declarations: [ LoanDoesNotExistComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
