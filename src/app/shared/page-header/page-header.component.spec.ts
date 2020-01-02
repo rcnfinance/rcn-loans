35ac9c9672ca89a2628c95b7220a07638c57ea95
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-01-02 11:32:08 -0300

    update page header unit test

--- src/app/shared/page-header/page-header.component.spec.ts
+++ src/app/shared/page-header/page-header.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { PageHeaderComponent } from './page-header.component';
+import { readComponent } from '../../utils/utils.test';
 
 describe('PageHeaderComponent', () => {
   let component: PageHeaderComponent;
@@ -8,7 +9,8 @@ describe('PageHeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PageHeaderComponent ]
+      declarations: [ PageHeaderComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
@@ -21,5 +23,41 @@ describe('PageHeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display title and description only', () => {
+    const expectedTitle = 'Activity';
+    const expectedDescription = 'Check the marketplace´s active loan';
+
+    component.title = expectedTitle;
+    component.description = expectedDescription;
+    fixture.detectChanges();
+
+    const title = readComponent(fixture, '.title');
+    expect(title.innerText).toBe(expectedTitle.toUpperCase());
+
+    const description = readComponent(fixture, '.subtitle');
+    expect(description.innerText).toBe(expectedDescription);
+
+    const chip = readComponent(fixture, '.mat-chip');
+    expect(chip).toBeUndefined();
+  });
+
+  it('should display title, description and chip amount', () => {
+    const expectedTitle = 'Activity';
+    const expectedDescription = 'Check the marketplace´s active loan';
+    const expectedChipValue = 15;
+    const expectedChipLabel = 'ACTIVE';
+
+    component.title = expectedTitle;
+    component.description = expectedDescription;
+    component.hasChip = true;
+    component.chipValue = expectedChipValue;
+    component.chipLabel = expectedChipLabel;
+    fixture.detectChanges();
+
+    const chip = readComponent(fixture, '.mat-chip');
+    expect(chip).toBeDefined();
+    expect(chip.innerText).toBe(`${ expectedChipValue } ${ expectedChipLabel}`);
   });
 });
