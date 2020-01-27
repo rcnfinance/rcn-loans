3529344f1f588f2a0906ca0c1eebf693b714040a
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-01-27 16:08:12 -0300

    mockup wallet dialog

--- src/app/dialogs/dialog-wallet-select/dialog-wallet-select.component.ts
+++ src/app/dialogs/dialog-wallet-select/dialog-wallet-select.component.ts
@@ -9,12 +9,39 @@ import { WalletType } from './../../interfaces/wallet.interface';
 })
 export class DialogWalletSelectComponent implements OnInit {
 
+  wallets: {
+    image: string;
+    title: string;
+    type: WalletType;
+    active: boolean;
+  }[];
+
   constructor(
     private dialogRef: MatDialogRef<DialogWalletSelectComponent>,
     public dialog: MatDialog
   ) { }
 
   ngOnInit() {
+    this.wallets = [
+      {
+        image: 'assets/logo-metamask.svg',
+        title: 'Metamask',
+        type: WalletType.Metamask,
+        active: true
+      },
+      {
+        image: 'assets/logo-coinbase.png',
+        title: 'Coinbase Wallet',
+        type: WalletType.WalletLink,
+        active: false
+      },
+      {
+        image: 'assets/logo-walletconnect.svg',
+        title: 'WalletConnect',
+        type: WalletType.WalletConnect,
+        active: false
+      }
+    ];
   }
 
   selectWallet(wallet: WalletType) {
