6b5f69a0938c566154e00ff7a10dd82feaf88791
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-03-09 18:27:00 -0300

    autocomplete loan on edit. add spinner

--- src/app/views/create-loan/step-create-loan/step-create-loan.component.ts
+++ src/app/views/create-loan/step-create-loan/step-create-loan.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 import { MatSnackBar } from '@angular/material';
+import { NgxSpinnerService } from 'ngx-spinner';
 import * as BN from 'bn.js';
 import { Utils } from '../../../utils/utils';
 import { LoanUtils } from '../../../utils/loan-utils';
@@ -22,22 +24,26 @@ import { Tx } from './../../../services/tx.service';
 })
 export class StepCreateLoanComponent implements OnInit, OnChanges {
 
-  loan: Loan;
-  @Input() account: string;
-  @Input() createPendingTx: Tx; // TODO: implement
-  @Output() updateLoan = new EventEmitter<Loan>();
-  @Output() createLoan = new EventEmitter<{ loan: Loan, form: LoanRequest }>();
-  @Output() loanWasCreated = new EventEmitter(); // TODO: implement
+  pageId = 'step-create-loan';
   durationDays: number[] = [15, 30, 45, 60, 75, 90];
   currencies: CurrencyItem[];
   daySeconds: BN = Utils.bn(60 * 60 * 24);
   installmentsFrequency: BN = Utils.bn(15);
 
+  @Input() account: string;
+  @Input() createPendingTx: Tx;
+  @Output() updateLoan = new EventEmitter<Loan>();
+  @Output() createLoan = new EventEmitter<{ loan: Loan, form: LoanRequest }>();
+  @Output() loanWasCreated = new EventEmitter(); // TODO: implement
+  loan: Loan;
   form: FormGroup;
 
   constructor(
     private snackBar: MatSnackBar,
+    private route: ActivatedRoute,
     private router: Router,
+    private location: Location,
+    private spinner: NgxSpinnerService,
     private web3Service: Web3Service,
     private contractsService: ContractsService,
     private currenciesService: CurrenciesService
@@ -47,6 +53,9 @@ export class StepCreateLoanComponent implements OnInit, OnChanges {
     this.buildForm();
     this.getCurrencies();
     this.updateLoanMockup();
+
+    const loanId: string = this.route.snapshot.params.id;
+    this.autocompleteForm(loanId);
   }
 
   async ngOnChanges() {
@@ -172,132 +181,205 @@ export class StepCreateLoanComponent implements OnInit, OnChanges {
     });
 
     this.form.controls.formUi.valueChanges.subscribe(async (formUi) => {
-      const amount: number = formUi.amount;
-      const currency: CurrencyItem = formUi.currency;
-      const duration: number = formUi.duration;
-      const expiration: number = formUi.expiration;
-      const annualInterestRate: number = formUi.annualInterestRate;
-      const installmentsActivated: boolean = formUi.installmentsActivated;
-      const DAY_SECONDS: BN = this.daySeconds;
-      const INSTALLMENTS_FREQUENCY = this.installmentsFrequency;
+      await this.updateFormUi(formUi);
+    });
+  }
 
-      // set oracle
-      if (currency) {
-        const oracle: string =
-          await this.contractsService.symbolToOracle(currency.symbol) ||
-          Utils.address0x;
+  private async updateFormUi(formUi) {
+    const amount: number = formUi.amount;
+    const currency: CurrencyItem = formUi.currency;
+    const duration: number = formUi.duration;
+    const expiration: number = formUi.expiration;
+    const annualInterestRate: number = formUi.annualInterestRate;
+    const installmentsActivated: boolean = formUi.installmentsActivated;
+    const DAY_SECONDS: BN = this.daySeconds;
+    const INSTALLMENTS_FREQUENCY = this.installmentsFrequency;
+    const DEFAULT_INSTALLMENTS = 1;
 
-        this.form.controls.formLoan.patchValue({ oracle });
-      } else {
-        this.form.controls.formLoan.patchValue({ oracle: null });
-      }
+    // set oracle
+    if (currency) {
+      const oracle: string =
+        await this.contractsService.symbolToOracle(currency.symbol) ||
+        Utils.address0x;
 
-      // set amount
-      if (currency && amount) {
+      this.form.controls.formLoan.patchValue({ oracle });
+    } else {
+      this.form.controls.formLoan.patchValue({ oracle: null });
+    }
+
+    // set amount
+    if (currency && amount) {
+      const { decimals } = new Currency(currency.symbol);
+      const amountInWei: BN = Utils.bn(amount).mul(Utils.pow(10, decimals));
+
+      this.form.controls.formLoan.patchValue({
+        amount: amountInWei.toString()
+      });
+    } else {
+      this.form.controls.formLoan.patchValue({ amount: null });
+    }
+
+    // set duration or frequency timestamp
+    if (duration) {
+      const durationSeconds: BN = Utils.bn(duration).mul(DAY_SECONDS);
+
+      this.form.controls.formModel.patchValue({
+        duration: durationSeconds.toString(),
+        installments: DEFAULT_INSTALLMENTS
+      });
+
+      // set available installemtns and replace duration by frequency
+      if (currency && amount && annualInterestRate) {
+        const availableInstallments: BN = Utils.bn(duration).div(INSTALLMENTS_FREQUENCY);
         const { decimals } = new Currency(currency.symbol);
         const amountInWei: BN = Utils.bn(amount).mul(Utils.pow(10, decimals));
-
-        this.form.controls.formLoan.patchValue({
-          amount: amountInWei.toString()
-        });
-      } else {
-        this.form.controls.formLoan.patchValue({ amount: null });
-      }
-
-      // set duration or frequency timestamp
-      if (duration) {
-        const durationSeconds: BN = Utils.bn(duration).mul(DAY_SECONDS);
-
-        this.form.controls.formModel.patchValue({
-          duration: durationSeconds.toString(),
-          installments: DEFAULT_INSTALLMENTS
-        });
-
-        // set available installemtns and replace duration by frequency
-        if (currency && amount && annualInterestRate) {
-          const availableInstallments: BN = Utils.bn(duration).div(INSTALLMENTS_FREQUENCY);
-          const { decimals } = new Currency(currency.symbol);
-          const amountInWei: BN = Utils.bn(amount).mul(Utils.pow(10, decimals));
-          const interestRate: number = Utils.toInterestRate(annualInterestRate);
-
-          let installments: BN = Utils.bn(DEFAULT_INSTALLMENTS);
-          let frequency: BN = durationSeconds;
-          if (Utils.bn(duration) > INSTALLMENTS_FREQUENCY && installmentsActivated) {
-            installments = availableInstallments;
-            frequency = durationSeconds.div(availableInstallments);
-          }
-
-          const estimatedCuota: BN = this.estimateCuotaAmount(
-            amountInWei,
-            installments,
-            Utils.bn(annualInterestRate),
-            Utils.bn(duration)
-          );
-
-          this.form.controls.formModel.patchValue({
-            duration: frequency.toString(),
-            installments: installments.toString(),
-            cuota: estimatedCuota.toString()
-          });
-
-          const loanData: string = await this.contractsService.encodeInstallmentsData(
-            estimatedCuota.toString(),
-            Utils.bn(interestRate).toString(),
-            installments.toString(),
-            frequency.toString(),
-            DAY_SECONDS.toString()
-          );
-
-          this.form.controls.formLoan.patchValue({
-            loanData
-          });
-        } else {
-          const loanData = null;
-          this.form.controls.formLoan.patchValue({
-            loanData
-          });
-        }
-      } else {
-        this.form.controls.formModel.patchValue({
-          duration: null,
-          installments: null,
-          cuota: null
-        });
-        this.form.controls.formLoan.patchValue({
-          loanData: null
-        });
-      }
-
-      // set annual interest rate and expected return
-      if (annualInterestRate) {
         const interestRate: number = Utils.toInterestRate(annualInterestRate);
 
-        this.form.controls.formModel.patchValue({
-          interestRate: Utils.bn(interestRate).toString()
-        });
-      } else {
-        this.form.controls.formModel.patchValue({
-          interestRate: null
-        });
-      }
+        let installments: BN = Utils.bn(DEFAULT_INSTALLMENTS);
+        let frequency: BN = durationSeconds;
+        if (Utils.bn(duration) > INSTALLMENTS_FREQUENCY && installmentsActivated) {
+          installments = availableInstallments;
+          frequency = durationSeconds.div(availableInstallments);
+        }
 
-      // set loan expiration date
-      if (expiration) {
-        const now: BN = Utils.bn(new Date().getTime()).div(Utils.bn(1000));
-        const expirationSeconds: BN = Utils.bn(expiration).mul(DAY_SECONDS);
-        const expirationTimestamp: BN = now
-            .add(expirationSeconds)
-            .add(Utils.bn(1));
+        const estimatedCuota: BN = this.estimateCuotaAmount(
+          amountInWei,
+          installments,
+          Utils.bn(annualInterestRate),
+          Utils.bn(duration)
+        );
+
+        this.form.controls.formModel.patchValue({
+          duration: frequency.toString(),
+          installments: installments.toString(),
+          cuota: estimatedCuota.toString()
+        });
+
+        const loanData: string = await this.contractsService.encodeInstallmentsData(
+          estimatedCuota.toString(),
+          Utils.bn(interestRate).toString(),
+          installments.toString(),
+          frequency.toString(),
+          DAY_SECONDS.toString()
+        );
 
         this.form.controls.formLoan.patchValue({
-          expiration: expirationTimestamp.toString()
+          loanData
         });
       } else {
-        this.form.controls.formLoan.patchValue({ expiration: null });
+        const loanData = null;
+        this.form.controls.formLoan.patchValue({
+          loanData
+        });
+      }
+    } else {
+      this.form.controls.formModel.patchValue({
+        duration: null,
+        installments: null,
+        cuota: null
+      });
+      this.form.controls.formLoan.patchValue({
+        loanData: null
+      });
+    }
+
+    // set annual interest rate and expected return
+    if (annualInterestRate) {
+      const interestRate: number = Utils.toInterestRate(annualInterestRate);
+
+      this.form.controls.formModel.patchValue({
+        interestRate: Utils.bn(interestRate).toString()
+      });
+    } else {
+      this.form.controls.formModel.patchValue({
+        interestRate: null
+      });
+    }
+
+    // set loan expiration date
+    if (expiration) {
+      const now: BN = Utils.bn(new Date().getTime()).div(Utils.bn(1000));
+      const expirationSeconds: BN = Utils.bn(expiration).mul(DAY_SECONDS);
+      const expirationTimestamp: BN = now
+          .add(expirationSeconds)
+          .add(Utils.bn(1));
+
+      this.form.controls.formLoan.patchValue({
+        expiration: expirationTimestamp.toString()
+      });
+    } else {
+      this.form.controls.formLoan.patchValue({ expiration: null });
+    }
+
+    const loan: Loan = await this.updateLoanMockup();
+    return loan;
+  }
+
+  /**
+   * Autocomplete form for handle an existing loan
+   * @param loan Loan
+   */
+  private async autocompleteForm(id?: string) {
+    if (!id) {
+      return;
+    }
+
+    try {
+      this.spinner.show(this.pageId);
+
+      const existingLoan: Loan = await this.getExistingLoan(id);
+      const account: string = this.account;
+      if (account && account.toLowerCase() !== existingLoan.creator.toLowerCase()) {
+        throw Error('User is not authorized');
       }
 
-      await this.updateLoanMockup();
-    });
+      const DAY_SECONDS = this.daySeconds;
+      const calculatedId: string = existingLoan.id;
+      const currency: CurrencyItem = this.currenciesService.getCurrencyByKey('symbol', existingLoan.currency.toString());
+      const annualInterestRate = existingLoan.descriptor.interestRate;
+      const decimals: number = existingLoan.currency.decimals;
+      const amount: number = existingLoan.amount / 10 ** decimals;
+      const duration: number = existingLoan.descriptor.duration / DAY_SECONDS.toNumber();
+      const installmentsActivated: boolean = existingLoan.descriptor.installments > 1;
+      const expirationSeconds: number = existingLoan.expiration - (new Date().getTime() / 1000);
+      const expiration: string = Number(expirationSeconds / DAY_SECONDS.toNumber()).toFixed(0);
+
+      this.form.controls.formUi.patchValue({
+        calculatedId,
+        amount,
+        currency,
+        duration,
+        expiration,
+        annualInterestRate,
+        installmentsActivated
+      });
+
+      const loan: Loan = await this.updateFormUi(this.form.value.formUi);
+      this.updateLoan.emit(loan);
+      this.loanWasCreated.emit();
+    } catch (e) {
+      this.location.replaceState(`/create`);
+      this.showMessage('Please create a new loan');
+      this.updateLoanMockup();
+    } finally {
+      this.spinner.hide(this.pageId);
+    }
+  }
+
+  /**
+   * Check if load exists
+   * @param id Loan ID
+   * @return Loan
+   */
+  private async getExistingLoan(id: string): Promise<Loan> {
+    const loan: Loan = await this.contractsService.getLoan(id);
+    const isRequest = loan.status === Status.Request;
+
+    if (!isRequest) {
+      throw Error('Loan is not on request status');
+    }
+
+    return loan;
   }
 
   /**
