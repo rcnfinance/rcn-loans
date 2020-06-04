ddf2603e070c7f729900485b9d92a41d6c2cdeda
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-06-04 17:36:10 -0300

    fix current collateral amount

--- src/app/services/collateral.service.ts
+++ src/app/services/collateral.service.ts
@@ -31,7 +31,8 @@ export class CollateralService {
     }
     const loanOracle: string = await this.contractsService.symbolToOracle(loan.currency.toString());
     const loanRate: BN | string = await this.contractsService.getRate(loanOracle, loan.currency.decimals);
-    const loanAmountInRcn: BN = Utils.bn(loan.amount)
+    const loanAmount: number = loan.debt ? loan.debt.model.estimatedObligation : loan.amount;
+    const loanAmountInRcn: BN = Utils.bn(loanAmount)
         .mul(Utils.bn(loanRate))
         .div(Utils.pow(10, loan.currency.decimals));
 
