a77fb6453a2b7bb661ffe80b7bdd3e09fad4c777
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-06-12 18:27:22 -0300

    mockup faqs

--- src/app/views/faqs/faqs.component.ts
+++ src/app/views/faqs/faqs.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+interface Faq {
+  id: number;
+  question: string;
+  answer: string;
+}
+interface FaqGroup {
+  title: string;
+  faqs: Faq[];
+}
 
 @Component({
   selector: 'app-faqs',
@@ -6,6 +15,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faqs.component.scss']
 })
 export class FaqsComponent implements OnInit {
+  faqs: Faq[];
+  faqGroups: FaqGroup[];
+  panelOpenState: {[id: number]: boolean} = {};
 
   constructor() { }
 
--- src/app/views/faqs/faqs.module.ts
+++ src/app/views/faqs/faqs.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from './../../shared/shared.module';
 import { FaqsComponent } from './faqs.component';
 
 const routes: Routes = [
@@ -11,7 +12,8 @@ const routes: Routes = [
   declarations: [FaqsComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule
   ]
 })
 export class FaqsModule { }
