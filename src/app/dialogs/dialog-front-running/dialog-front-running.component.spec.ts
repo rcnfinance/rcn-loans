d7c4688d057afa58d4b6a7df4eda9003c32c09f2
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-06-19 16:36:48 -0300

    update dialog front running unit test

--- src/app/dialogs/dialog-front-running/dialog-front-running.component.spec.ts
+++ src/app/dialogs/dialog-front-running/dialog-front-running.component.spec.ts
@@ -1,14 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MaterialModule } from './../../material.module';
 import { DialogFrontRunningComponent } from './dialog-front-running.component';
 
 describe('DialogFrontRunningComponent', () => {
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
   let component: DialogFrontRunningComponent;
   let fixture: ComponentFixture<DialogFrontRunningComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DialogFrontRunningComponent ]
+      imports: [ MaterialModule, MatDialogModule ],
+      declarations: [ DialogFrontRunningComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: mockDialogRef
+        }
+      ]
     })
     .compileComponents();
   }));
