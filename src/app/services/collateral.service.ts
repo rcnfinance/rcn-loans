abb8f4a8425bb478152e56c06da2f137c948c32e
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-07-22 02:00:55 -0300

    remove unused method

--- src/app/services/collateral.service.ts
+++ src/app/services/collateral.service.ts
@@ -52,42 +52,6 @@ export class CollateralService {
   }
 
   /**
-   * Calculate collateral liquidation price
-   * @param loanId Loan ID
-   * @param collateralRate Collateral rate
-   * @param liquidationRatio Liquidation ratio
-   * @param loanAmountInRcn Loan amount in rcn
-   * @return Liquidation price in collateral amount
-   */
-  async calculateLiquidationPrice(
-    loanId: string,
-    collateralRate,
-    liquidationRatio,
-    loanAmountInRcn
-  ) {
-    collateralRate = Utils.bn(collateralRate);
-    liquidationRatio = Utils.bn(liquidationRatio);
-
-    let amountInRcn: BN | string;
-
-    try {
-      const debt = await this.contractsService.getClosingObligation(loanId);
-      amountInRcn = Utils.bn(debt || 0);
-    } catch (e) {
-      amountInRcn = Utils.bn(loanAmountInRcn);
-    }
-
-    try {
-      let liquidationPrice = Utils.bn(liquidationRatio).mul(amountInRcn).div(Utils.bn(100));
-      liquidationPrice = liquidationPrice.div(collateralRate);
-
-      return liquidationPrice;
-    } catch (e) {
-      return null;
-    }
-  }
-
-  /**
    * Check if the sended transaction is from the current collateral
    * @param tx Tx
    * @param collateralId Collateral ID
