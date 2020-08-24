f301147e61a2104c42765162a95d1cfb5103b5b0
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-08-24 18:00:11 -0300

    fix installments-service unit test imports

--- src/app/services/installments.service.spec.ts
+++ src/app/services/installments.service.spec.ts
@@ -1,9 +1,17 @@
-import { TestBed } from '@angular/core/testing';
-
+import { async, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { InstallmentsService } from './installments.service';
+import { CommitsService } from './commits.service';
+import { EventsService } from './events.service';
 
 describe('InstallmentsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [InstallmentsService, CommitsService, EventsService]
+    })
+    .compileComponents();
+  }));
 
   it('should be created', () => {
     const service: InstallmentsService = TestBed.get(InstallmentsService);
