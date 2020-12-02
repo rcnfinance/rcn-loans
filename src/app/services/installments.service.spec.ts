25423a3ff78ea17d46c91dca78e7365956cbcfd5
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-12-02 14:12:46 -0300

    fix unit test import

--- src/app/services/installments.service.spec.ts
+++ src/app/services/installments.service.spec.ts
@@ -1,14 +1,13 @@
 import { async, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { InstallmentsService } from './installments.service';
-import { CommitsService } from './commits.service';
 import { EventsService } from './events.service';
 
 describe('InstallmentsService', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      providers: [InstallmentsService, CommitsService, EventsService]
+      providers: [InstallmentsService, EventsService]
     })
     .compileComponents();
   }));
