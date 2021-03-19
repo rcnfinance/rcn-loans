89c2b00f9e25e36726955a2f26d3f2fb5becdc43
Nicolás Acosta <nadesarrolloweb@gmail.com>
2021-03-19 14:15:27 -0300

    prepare unit test files

--- src/app/dialogs/dialog-poh/dialog-poh.component.spec.ts
+++ src/app/dialogs/dialog-poh/dialog-poh.component.spec.ts
@@ -1,14 +1,31 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MaterialModule } from 'app/material.module';
+import { EventsService } from 'app/services/events.service';
 import { DialogPohComponent } from './dialog-poh.component';
 
 describe('DialogPohComponent', () => {
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
+
   let component: DialogPohComponent;
   let fixture: ComponentFixture<DialogPohComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DialogPohComponent ]
+      imports: [ HttpClientModule, MaterialModule, MatDialogModule ],
+      declarations: [ DialogPohComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: mockDialogRef
+        },
+        EventsService
+      ]
     })
     .compileComponents();
   }));
--- src/app/services/poh.service.spec.ts
+++ src/app/services/poh.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { PohService } from './poh.service';
 
 describe('PohService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule]
+  }));
 
   it('should be created', () => {
     const service: PohService = TestBed.get(PohService);
