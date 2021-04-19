2e39c9cb4ecb664effaf9903f3700ad428c32636
Martin Bobbio <martinbobbio@MacBook-Pro-de-To.local>
2021-04-19 15:32:08 -0300

    adding dashboard list component

---
+++ src/app/shared/dashboard-list/dashboard-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-list',
+  templateUrl: './dashboard-list.component.html',
+  styleUrls: ['./dashboard-list.component.scss']
+})
+export class DashboardListComponent implements OnInit {
+
+  @Input() title: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
