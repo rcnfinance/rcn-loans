7f510871b7275c83d78d879c959e5fcb9a8832c6
Martin Bobbio <martinbobbio@MBP-de-To.fibertel.com.ar>
2021-05-12 17:10:51 -0300

    animation in home with scroll

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  HostListener,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material';
 import { animate, style, transition, trigger } from '@angular/animations';
 import { Subscription } from 'rxjs';
 import { Web3Service } from 'app/services/web3.service';
 import { TitleService } from 'app/services/title.service';
+import { DeviceService } from 'app/services/device.service';
 import { WalletConnectService } from 'app/services/wallet-connect.service';
 import { WalletType } from 'app/interfaces/wallet.interface';
 import { DialogWalletSelectComponent } from 'app/dialogs/dialog-wallet-select/dialog-wallet-select.component';
@@ -15,32 +23,32 @@ import { DialogWalletSelectComponent } from 'app/dialogs/dialog-wallet-select/di
   styleUrls: ['./home.component.scss'],
   animations: [
     trigger('fadeInSlow', [
-      transition('void => *', [
+      transition('* => true', [
         style({ opacity: 0 }),
         animate(2000, style({ opacity: 1 }))
       ])
     ]),
     trigger('fadeInFast', [
-      transition('void => *', [
+      transition('* => true', [
         style({ opacity: 0 }),
         animate(1000, style({ opacity: 1 }))
       ])
     ]),
     trigger('fadeInRight', [
-      transition('* => *', [
-        style({ opacity: .5, transform: 'translateX(100%)' }),
+      transition('* => true', [
+        style({ opacity: 0.5, transform: 'translateX(100%)' }),
         animate(1000, style({ transform: 'translateX(0%)', opacity: 1 }))
       ])
     ]),
     trigger('fadeInLeft', [
-      transition('* => *', [
-        style({ opacity: .5, transform: 'translateX(-100%)' }),
-        animate(1000, style({ transform: 'translateX(0%)', opacity: 1 }))
+      transition('* => true', [
+        style({ opacity: 0.5, transform: 'translateX(-100%)', transitionDelay: 2000 }),
+        animate(1000, style({ transform: 'translateX(0%)', opacity: 1, transitionDelay: 2000 }))
       ])
     ]),
     trigger('fadeInBottom', [
-      transition('* => *', [
-        style({ opacity: .5, transform: 'translateY(-100%)' }),
+      transition('* => true', [
+        style({ opacity: 0.5, transform: 'translateY(-100%)' }),
         animate(1000, style({ transform: 'translateY(0%)', opacity: 1 }))
       ])
     ])
@@ -50,18 +58,54 @@ export class HomeComponent implements OnInit, OnDestroy {
   private account: string;
   private subscriptionAccount: Subscription;
 
+  animationSectionIntro = true;
+  animationSectionConnect = false;
+  animationSectionManage = false;
+  animationSectionLend = false;
+  animationSectionBorrow = false;
+
+  @ViewChild('sectionConnect', { static: false }) sectionConnect: ElementRef;
+  @ViewChild('sectionManage', { static: false }) sectionManage: ElementRef;
+  @ViewChild('sectionLend', { static: false }) sectionLend: ElementRef;
+  @ViewChild('sectionBorrow', { static: false }) sectionBorrow: ElementRef;
+
   constructor(
     private router: Router,
     private dialog: MatDialog,
     private web3Service: Web3Service,
     private titleService: TitleService,
-    private walletConnectService: WalletConnectService
-  ) { }
+    private walletConnectService: WalletConnectService,
+    private deviceService: DeviceService
+  ) {}
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const tolerancePixels = this.deviceService.isMobile() ? 850 : 1000;
+    const scrollPosition = window.pageYOffset + tolerancePixels;
+    const sectionConnectPosition = this.sectionConnect.nativeElement.offsetTop;
+    const sectionManagePosition = this.sectionManage.nativeElement.offsetTop;
+    const sectionLendPosition = this.sectionLend.nativeElement.offsetTop;
+    const sectionBorrowPosition = this.sectionBorrow.nativeElement.offsetTop;
+
+    if (scrollPosition >= sectionConnectPosition) {
+      this.animationSectionConnect = true;
+    }
+    if (scrollPosition >= sectionManagePosition) {
+      this.animationSectionManage = true;
+    }
+    if (scrollPosition >= sectionLendPosition) {
+      this.animationSectionLend = true;
+    }
+    if (scrollPosition >= sectionBorrowPosition) {
+      this.animationSectionBorrow = true;
+    }
+  }
 
   ngOnInit() {
     this.titleService.changeTitle('RCN');
     this.loadAccount();
     this.listenLoginEvents();
+    this.checkAnimations();
   }
 
   ngOnDestroy() {
@@ -89,10 +133,11 @@ export class HomeComponent implements OnInit, OnDestroy {
    * Listen and handle login events for account changes and logout
    */
   private listenLoginEvents() {
-    this.subscriptionAccount =
-        this.web3Service.loginEvent.subscribe(async () => {
-          this.loadAccount();
-        });
+    this.subscriptionAccount = this.web3Service.loginEvent.subscribe(
+      async () => {
+        this.loadAccount();
+      }
+    );
   }
 
   /**
@@ -101,5 +146,14 @@ export class HomeComponent implements OnInit, OnDestroy {
   private async loadAccount() {
     const account = await this.web3Service.getAccount();
     this.account = account;
+  }
+
+  /**
+   * Check mobile screen to adjust animations
+   */
+  private async checkAnimations() {
+    if (this.deviceService.isMobile()) {
+      this.animationSectionLend = true;
+    }
   }
 }
