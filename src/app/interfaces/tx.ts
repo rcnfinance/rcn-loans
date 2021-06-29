9adc0947a2eae97dc03d1020c34baddccfe8bb19
Nicolás Acosta <nadesarrolloweb@gmail.com>
2021-06-29 17:51:14 -0300

    add new tx models/interfaces

---
+++ src/app/interfaces/tx.ts
@@ -0,0 +1,13 @@
+export enum Type {
+  lend = 'lend',
+  approve = 'approve',
+  withdraw = 'withdraw',
+  transfer = 'transfer',
+  claim = 'claim',
+  pay = 'pay',
+  create = 'create',
+  createCollateral = 'createCollateral',
+  addCollateral = 'addCollateral',
+  withdrawCollateral = 'withdrawCollateral',
+  redeemCollateral = 'redeemCollateral'
+}
